57493dcb3593f5ca900904ff20e31dbc92102c8a
Pieter <pieter.schutte@structureit.net>
2014-03-25 11:57:35 +0200

    Tests: Refactor Nancy module tests

---
+++ app/Workflow/Components/Billing/Billing.Api.Tests/Transaction/TransactionModuleTestBase.cs
@@ -0,0 +1,23 @@
+﻿using Nancy.Testing;
+using Workflow;
+using Xunit.Extensions;
+
+namespace Billing.Api.Tests.Transaction
+{
+    public abstract class TransactionModuleTestBase : Specification
+    {
+        protected readonly Browser browser;
+        protected readonly IPublishMessages publisher;
+        protected const string url = "/transaction";
+
+        protected TransactionModuleTestBase(IPublishMessages publisher)
+        {
+            this.publisher = publisher;
+            browser = new Browser(with =>
+            {
+                with.Module<Api.Transaction>();
+                with.Dependency(publisher);
+            });
+        }
+    }
+}
--- app/Workflow/Components/Billing/Billing.Api.Tests/Transaction/when_creating_a_new_billing_transaction.cs
+++ app/Workflow/Components/Billing/Billing.Api.Tests/Transaction/when_creating_a_new_billing_transaction.cs
@@ -1,36 +1,32 @@
-﻿using Billing.Api.Dtos;
+﻿using System;
+using Billing.Api.Dtos;
 using Billing.Api.Tests.Fakes;
 using Billing.Api.Tests.Mothers.CreatTransactionDto;
 using Nancy;
 using Nancy.Testing;
-using Workflow;
 using Workflow.Billing.Messages;
 using Xunit.Extensions;
 
-namespace Billing.Api.Tests
+namespace Billing.Api.Tests.Transaction
 {
-    public class when_creating_a_new_billing_transaction : Specification
+    public class when_creating_a_new_billing_transaction : TransactionModuleTestBase
     {
-        private readonly Browser browser;
         private BrowserResponse result;
         private readonly CreateTransaction transaction;
-        private readonly TestMessagePublisher publisher;
 
-        public when_creating_a_new_billing_transaction()
+        public when_creating_a_new_billing_transaction() : base(new TestMessagePublisher())
         {
-            publisher = new TestMessagePublisher();
-
-            browser = new Browser(with =>
-                                  {
-                                      with.Module<Transaction>();
-                                      with.Dependency<IPublishMessages>(publisher);
-                                  });
             transaction = new CreateTransactionMother().DefaultCreateTransaction();
         }
 
         public override void Observe()
         {
-            result = browser.Post("/transaction", with => with.JsonBody(transaction));
+            result = browser.Post(url, with => with.JsonBody(transaction));
+        }
+
+        private TestMessagePublisher ThePublisher
+        {
+            get { return publisher as TestMessagePublisher; }
         }
 
         [Observation]
@@ -42,14 +38,14 @@ namespace Billing.Api.Tests
         [Observation]
         public void a_message_is_published_to_create_the_billing_transaction()
         {
-            publisher.PublishedMessage.ShouldNotBeNull();
-            publisher.PublishedMessage.ShouldBeType<BillTransactionMessage>();
+            ThePublisher.PublishedMessage.ShouldNotBeNull();
+            ThePublisher.PublishedMessage.ShouldBeType<BillTransactionMessage>();
         }
 
         [Observation]
         public void the_published_message_has_the_correct_information()
         {
-            var message = publisher.PublishedMessage as BillTransactionMessage;
+            var message = ThePublisher.PublishedMessage as BillTransactionMessage;
 
             message.TransactionId.ShouldEqual(transaction.TransactionId);
             message.UserId.ShouldEqual(transaction.UserId);
