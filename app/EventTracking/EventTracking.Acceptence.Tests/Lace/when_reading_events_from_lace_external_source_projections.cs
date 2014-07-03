6c710a59145ecf100875026ea60f32bbd169e20b
Rudi Jonker <rudi.jonker@structureit.net>
2014-07-03 17:48:46 +0200

    Event Tracking

--- app/EventTracking/EventTracking.Acceptence.Tests/Lace/when_reading_events_from_lace_external_source_projections.cs
+++ app/EventTracking/EventTracking.Acceptence.Tests/Lace/when_reading_events_from_lace_external_source_projections.cs
@@ -12,40 +12,44 @@ namespace EventTracking.Acceptence.Tests
     public class when_reading_events_from_lace_external_source_projections : Specification
     {
 
-        private readonly EventReader _eventReader;
-        private readonly IProjectionContext _projectionContext;
-        private readonly IEventStoreConnection _connection;
-        private readonly ExternalSourceEventDetectorProjection _sourceRequestsProjection;
-        private const string StreamCategoryName = "ExternalSourceEvents";
+        //private readonly EventReader _eventReader;
+        //private readonly IProjectionContext _projectionContext;
+        //private readonly IEventStoreConnection _connection;
+        //private readonly ExternalSourceEventDetectorProjection _sourceRequestsProjection;
+        //private const string StreamCategoryName = "ExternalSourceEvents";
 
 
-        public when_reading_events_from_lace_external_source_projections()
-        {
-            _projectionContext = new ProjectionContext();
-            _sourceRequestsProjection = new ExternalSourceEventDetectorProjection(_projectionContext);
-            _connection = ConnectionFactory.Default();
-            _eventReader = new EventReader(_connection);
-        }
+        //public when_reading_events_from_lace_external_source_projections()
+        //{
+        //    _projectionContext = new ProjectionContext();
+        //    _sourceRequestsProjection = new ExternalSourceEventDetectorProjection(_projectionContext);
+        //    _connection = ConnectionFactory.Default();
+        //    _eventReader = new EventReader(_connection);
+        //}
+
+        //public override void Observe()
+        //{
+        //    //_projectionContext.EnableProjection("$by_category");
+        //    //_projectionContext.EnableProjection("$stream_by_category");
+
+        //    //_sourceRequestsProjection.Ensure();
+
+        //    //_eventReader.StartReading();
+        //}
+
+        //[Observation]
+        //public void projection_for_lace_external_source_must_exist()
+        //{
+        //    var results = _connection.ReadStreamEventsBackward<ExternalSourceEventRead>(StreamCategoryName);
+        //    results.Count().ShouldNotEqual(0);
+        //}
+
+
+
 
         public override void Observe()
         {
-            //_projectionContext.EnableProjection("$by_category");
-            //_projectionContext.EnableProjection("$stream_by_category");
-
-            //_sourceRequestsProjection.Ensure();
-
-            //_eventReader.StartReading();
+            
         }
-
-        [Observation]
-        public void projection_for_lace_external_source_must_exist()
-        {
-            var results = _connection.ReadStreamEventsBackward<ExternalSourceEventRead>(StreamCategoryName);
-            results.Count().ShouldNotEqual(0);
-        }
-
-
-
-
     }
 } 
