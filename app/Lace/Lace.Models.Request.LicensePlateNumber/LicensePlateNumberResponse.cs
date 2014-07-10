510a6fc0d35b66e61ce2cce14314e16766b21cc5
Rudi Jonker <rudi.jonker@structureit.net>
2014-07-10 16:21:31 +0200

    LACE
    Removed old projects

--- app/Lace/Lace.Models.Request.LicensePlateNumber/LicensePlateNumberResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Lace.Response;
-
-namespace Lace.Models.Request.LicensePlateNumber
-{
-    public class LicensePlateNumberResponse
-    {
-        public ILaceResponse Response { get; set; } 
-    }
-}
