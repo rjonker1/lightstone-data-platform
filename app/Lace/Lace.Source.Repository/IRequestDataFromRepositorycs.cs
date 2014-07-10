40abb3e34af70cb31c573684cb1a1e856405fef3
Rudi Jonker <rudi.jonker@structureit.net>
2014-07-10 17:40:07 +0200

    Lace
    Added solution and removed not used projects

--- app/Lace/Lace.Source.Repository/IRequestDataFromRepositorycs.cs
+++
@@ -1,9 +0,0 @@
-﻿using Lace.Response;
-
-namespace Lace.Source.Repository
-{
-    public interface IRequestProductDataFromRepository
-    {
-        void FetchDataFromRepository(ILaceResponse response, IGetProductDataFromRepository repository);
-    }
-}
