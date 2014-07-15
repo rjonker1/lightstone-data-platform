4ef7710a8dd28e9e1d0756e960d0a5d26e1108a3
Rudi Jonker <rudi.jonker@structureit.net>
2014-07-15 12:05:29 +0200

    Event Tracking

--- app/EventTracking/EventTracking.Measurement/Streams/ExternalSourceEventStreams.cs
+++ app/EventTracking/EventTracking.Measurement/Streams/ExternalSourceEventStreams.cs
@@ -1,10 +1,10 @@
-﻿using System;
-
-namespace EventTracking.Measurement.Streams
+﻿namespace EventTracking.Measurement.Streams
 {
     public class ExternalSourceEventStreams
     {
-        public static Func<string> ExternalSourceExecutionEvents = () => "ExternalSourceExecutionEvents";
-        public static Func<string> PublisherExternalSourceExecutionEventPublisherCheckpoint = () => "$publisher-ExternalSourceExecutionEventPublisher-checkpoint";
+        public const string ExternalSourceExecutionEvents = "ExternalSourceExecutionEvents";
+
+        public const string PublisherExternalSourceExecutionEventPublisherCheckpoint =
+            "$publisher-ExternalSourceExecutionEventPublisher-checkpoint";
     }
 }
