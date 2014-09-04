e22ec370544b9786f59a9cb5192ef6073ed72353
Rudi Jonker <rudi.jonker@structureit.net>
2014-09-04 14:40:27 +0200

    LACE
    Removed ALL unit tests

--- app/Lace/Lace.Test.Helper/Fakes/Lace/LicensePlateNumber/FakeLicensePlateNumberResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Lace.Response;
-
-namespace Lace.Test.Helper.Fakes.Lace.LicensePlateNumber
-{
-    public class FakeLicensePlateNumberResponse
-    {
-        public ILaceResponse Response { get; set; } 
-    }
-}
