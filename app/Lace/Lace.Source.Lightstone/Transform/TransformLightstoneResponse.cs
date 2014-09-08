28a56df6703303244575833ebaa7b75e99f7835b
Rudi Jonker <rudi.jonker@structureit.net>
2014-09-08 09:09:23 +0200

    LACE - Lightstone External Sources

--- app/Lace/Lace.Source.Lightstone/Transform/TransformLightstoneResponse.cs
+++ app/Lace/Lace.Source.Lightstone/Transform/TransformLightstoneResponse.cs
@@ -10,17 +10,15 @@ namespace Lace.Source.Lightstone.Transform
         public bool Continue { get; private set; }
         public LightstoneResponse Result { get; private set; }
         private readonly IRetrieveValuationFromMetrics _metricResponse;
-        private readonly IRetrieveCarDetailFromCarVendorInformation _carDetailResponse;
         private readonly IRetrieveCarInformation _carInformation;
 
         public TransformLightstoneResponse(IRetrieveValuationFromMetrics metricResponse,
-            IRetrieveCarDetailFromCarVendorInformation carDetailReponse, IRetrieveCarInformation carInformation)
+            IRetrieveCarInformation carInformation)
         {
             Continue = metricResponse != null && metricResponse.IsSatisfied;
             Result = Continue ? new LightstoneResponse() : null;
 
             _metricResponse = metricResponse;
-            _carDetailResponse = carDetailReponse;
             _carInformation = carInformation;
         }
 
