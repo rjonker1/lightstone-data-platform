b33582f4d9708ddf8f1c7cdf902d68b3e5506ca3
Allistaire Clark <allistairec@lightstone.co.za>
2014-09-19 16:13:00 +0200

    Aggregate functionality on field prices

--- app/PackageBuilder/PackageBuilder.Web.UI/app/scripts/controllers/packageDetailController.js
+++ app/PackageBuilder/PackageBuilder.Web.UI/app/scripts/controllers/packageDetailController.js
@@ -35,6 +35,61 @@ angular.module('packageBuilderwebuiApp')
                         }];
         });
 
+        $scope.dataPros = {};
+        $scope.dataPros.prods = {};
+        $scope.dataPros.prods.prod = {};
+        $scope.sum = 0;
+        
+        $scope.$watch('dataPros', function (newValue, oldValue) {
+            var findProdById = function (products, id) {
+                for(var prods in products){ /*{}*/
+
+
+                    if (products.hasOwnProperty(prods)) {
+                          
+                        var attr = products[prods];
+                        
+                        for (var i = 0; i < attr.length; i++) {        /*[]*/
+                                                                        
+                                /*for (var props in attr[i].fields) {*/                                             
+                                var dps = attr[i].fields;   
+                                /*$scope.test = dps[1].id;        */
+                                for (var x = 0; x < dps.length; x++) {
+                                    
+
+                                     if (dps[x].id == id) {
+                                                                     
+                                          return dps[x];
+                                      } 
+
+                                      /*return null;*/    
+                                }
+
+                        }
+                     }
+                                  
+                }         
+            };
+
+            var orders = newValue;
+               
+            var usedProducts = newValue.prods.prod;
+            var sum = 0;
+            for ( var prop in usedProducts ){
+
+               var dataProvList = $scope.dataProvsPkg;
+               var id = prop;
+               if ( usedProducts[id] ){
+                   
+                   var product = findProdById(dataProvList, id);
+                   if ( product !== null ){
+                       sum += parseInt(product.price, 10);
+                   }
+               }
+            }
+            $scope.sum = sum;
+        }, true);
+
     $scope.toggle = function(scope) {
       scope.toggle();
     };
