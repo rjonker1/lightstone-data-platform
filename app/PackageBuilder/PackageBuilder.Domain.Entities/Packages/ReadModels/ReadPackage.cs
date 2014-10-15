087406db6550d5dabbb331ee85ef4eb9a406175e
bakerbrian <brian@customapp.co.za>
2014-10-15 09:45:58 +0200

    Checked in missing files

---
+++ app/PackageBuilder/PackageBuilder.Domain.Entities/Packages/ReadModels/ReadPackage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PackageBuilder.Domain.Entities.Packages.ReadModels
+{
+    public class ReadPackage
+    {
+        public Guid Id { get; set; }
+        public Guid DataProviderId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Owner { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Edited { get; set; }
+        public int Version { get; set; }
+    }
+}
---
+++ app/PackageBuilder/PackageBuilder.Domain/EventHandlers/Packages/PackageUpdatedHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PackageBuilder.Domain.Entities.DataProviders.Events;
+using PackageBuilder.Domain.Entities.DataProviders.ReadModels;
+using PackageBuilder.Domain.MessageHandling;
+using Raven.Client;
+
+namespace PackageBuilder.Domain.EventHandlers.Packages
+{
+    public class PackageUpdatedHandler : AbstractMessageHandler<DataProviderUpdated>
+    {
+
+        private readonly IDocumentSession _session;
+
+        public PackageUpdatedHandler(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public override void Handle(DataProviderUpdated command)
+        {
+
+            Guid id = new Guid();
+
+            var update = new ReadDataProvider
+            {
+                Id = id,
+                DataProviderId = command.DataProvierId,
+                Name = command.Name,
+                Version = command.Version,
+                Created = command.Created,
+                Edited = command.Edited
+            };
+
+            _session.Store(update);
+            _session.SaveChanges();
+        }
+
+    }
+}
