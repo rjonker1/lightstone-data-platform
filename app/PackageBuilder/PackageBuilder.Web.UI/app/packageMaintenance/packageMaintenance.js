e1626c2b162618112cb5928ae8efa79350f38705
Allistaire Clark <allistairec@lightstone.co.za>
2014-10-17 16:15:22 +0200

    Base reference for ui-tree for Package Maintenance

--- app/PackageBuilder/PackageBuilder.Web.UI/app/packageMaintenance/packageMaintenance.js
+++ app/PackageBuilder/PackageBuilder.Web.UI/app/packageMaintenance/packageMaintenance.js
@@ -7,14 +7,27 @@
         .module('app')
         .controller(controllerId, packageMaintenance);
 
-    packageMaintenance.$inject = ['$scope', 'common', 'datacontext'];
+    packageMaintenance.$inject = ['$scope', '$http', 'common', 'datacontext'];
 
-    function packageMaintenance($scope, common, datacontext) {
+    function packageMaintenance($scope, $http, common, datacontext) {
 
         $scope.title = 'packageMaintenance';
         var getLogFn = common.logger.getLogFn;
         var log = getLogFn(controllerId);
         var logError = getLogFn(controllerId, 'error');
+
+        //MOCK
+        $http({
+            method: 'GET',
+            url: '/DataProviders.json'
+            }).success(function(data, status, headers, config) {
+                
+               $scope.dataProvsPkg = data;
+    
+            }).error(function(data, status, headers, config) {
+              
+             
+            });
 
         activate();
 
@@ -26,11 +39,11 @@
 
         function getDataProviders() {
 
-            return datacontext.getDataProviders().then(function (data) {
+            //return datacontext.getDataProviders().then(function (data) {
 
-                (data.indexOf('Error') > -1) ? (logError(data) ? $scope.test = null : $scope.test = data) : log('Data loaded successfully!');
+                //(data.indexOf('Error') > -1) ? (logError(data) ? $scope.test = null : $scope.test = data) : log('Data loaded successfully!');
 
-            });
+           // });
         }
     }
 })();
