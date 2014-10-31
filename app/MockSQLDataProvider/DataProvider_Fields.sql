f17069d556ace217f18aa95aa3137a8cfd9c3526
Rudi Jonker <rudi@customapp.co.za>
2014-10-31 09:10:13 +0200

    Removed Mock SQL Data Provider

--- app/MockSQLDataProvider/DataProvider_Fields.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[DataProvider_Fields]
-(
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(50) NULL, 
-    [Description] NVARCHAR(50) NULL, 
-    [Price] DECIMAL(10, 2) NULL, 
-    [Provider] NVARCHAR(50) NULL
-)
