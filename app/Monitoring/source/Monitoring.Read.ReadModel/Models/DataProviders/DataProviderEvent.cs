6ff256667ea6b6ba00059951ac761568a7e6a8c0
Rudi Jonker <rudi@customapp.co.za>
2014-11-06 08:00:03 +0200

    Merge with DEV

---
+++ app/Monitoring/source/Monitoring.Read.ReadModel/Models/DataProviders/DataProviderEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Monitoring.Read.ReadModel.Models.DataProviders
+{
+    public class DataProviderEvent : ReadModelEntity
+    {
+        public DataProviderEvent()
+        {
+        }
+
+        public DataProviderEvent(Guid id) : base(id)
+        {
+            
+        }
+
+        public DateTime TimeStamp { get; set; }
+        public int Source { get; set; }
+        public string Payload { get; set; }
+    }
+}
