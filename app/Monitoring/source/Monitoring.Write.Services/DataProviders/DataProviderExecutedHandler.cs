cb778302633bfa5147881cb578e901a07462ed95
Rudi Jonker <rudi@customapp.co.za>
2014-11-10 08:25:30 +0200

    added non tracked files

---
+++ app/Monitoring/source/Monitoring.Write.Services/DataProviders/DataProviderExecutedHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CommonDomain.Persistence;
+using Monitoring.Domain.Messages.Commands;
+using Monitoring.DomainModel.DataProviders;
+using NServiceBus;
+
+namespace Monitoring.Write.Service.DataProviders
+{
+    public class DataProviderExecutedHandler : IHandleMessages<ExecuteDataProvider>
+    {
+        private readonly IRepository _repository;
+
+        public DataProviderExecutedHandler()
+        {}
+
+        public DataProviderExecutedHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Handle(ExecuteDataProvider message)
+        {
+            var @event = new DataProviderExecution(message.Id, message.DataProviderId, message.Message, message.Date);
+            _repository.Save(@event, Guid.NewGuid(), null);
+        }
+    }
+}
