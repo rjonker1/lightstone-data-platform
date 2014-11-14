c56719e6979f622778a00227f0353b8cc377b516
Allistaire Clark <allistairec@lightstone.co.za>
2014-11-14 14:52:09 +0200

    Implemented IDataField extension mapping

--- app/PackageBuilder/PackageBuilder.Domain.Entities/DataFields/WriteModels/IDataField.cs
+++ app/PackageBuilder/PackageBuilder.Domain.Entities/DataFields/WriteModels/IDataField.cs
@@ -5,9 +5,12 @@ namespace PackageBuilder.Domain.Entities.DataFields.WriteModels
 {
     public interface IDataField : INamedEntity
     {
+        string Label { get; }
+        string Definition { get; }
+        string Industry { get; }
+        double Price { get; }
+        bool? IsSelected { get; }
         Type Type { get; }
         IEnumerable<IDataField> DataFields { get; }
-        string Industry { get; }
-        bool? IsSelected { get; }
     }
 }
