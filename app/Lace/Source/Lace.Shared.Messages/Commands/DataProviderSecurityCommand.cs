a2e14e49c2862fc38e3094b1a6218ab45c2d1f93
Rudi Jonker <rudi@customapp.co.za>
2014-12-02 17:06:48 +0200

    Added missing files

---
+++ app/Lace/Source/Lace.Shared.Messages/Commands/DataProviderSecurityCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Lace.Shared.Monitoring.Messages.Core;
+
+namespace Lace.Shared.Monitoring.Messages.Commands
+{
+    [Serializable]
+    public class DataProviderSecurityCommand
+    {
+        public Guid Id { get; private set; }
+        public DataProvider DataProvider { get; private set; }
+        public string Message { get; private set; }
+        public string Payload { get; private set; }
+        public DateTime Date { get; private set; }
+        public Category Category { get; private set; }
+        public string MetaData { get; private set; }
+        public bool IsJson { get; private set; }
+
+        public DataProviderSecurityCommand()
+        {
+
+        }
+
+        public DataProviderSecurityCommand(Guid id, DataProvider dataProvider, string message, string payload,
+            string metadata,
+            DateTime date, Category category, bool isJson)
+        {
+            Id = id;
+            DataProvider = dataProvider;
+            Message = message;
+            Payload = payload;
+            Date = date;
+            Category = category;
+            MetaData = metadata;
+            IsJson = isJson;
+        }
+    }
+}
