471b994ef7ff32b2be1a55208830d2ea9ebbe979
Rudi Jonker <rudi@customapp.co.za>
2014-12-05 08:18:49 +0200

    MONITORING: Added Missing Files

---
+++ app/Monitoring/source/Monitoring.DomainModel/DataProviders/DataProviderExecution.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using CommonDomain.Core;
+using Lace.Shared.Monitoring.Messages.Core;
+using Lace.Shared.Monitoring.Messages.Events;
+
+namespace Monitoring.DomainModel.DataProviders
+{
+    public class DataProviderAggregate : AggregateBase
+    {
+        private DataProviderAggregate(Guid id)
+        {
+            Id = id;
+            Register<DataProviderEvent>(e => Id = id);
+        }
+
+        public DataProviderAggregate(Guid id, DataProvider dataProvider, Category category, string message,
+            string payload, string metadata, DateTime date, bool isJson)
+            : this(id)
+        {
+            RaiseEvent(new DataProviderEvent(id, dataProvider, category, message, payload, metadata, date, isJson));
+        }
+    }
+
+    public class DataProviderFaultAggregate : AggregateBase
+    {
+        private DataProviderFaultAggregate(Guid id)
+        {
+            Id = id;
+            Register<FaultInDataProviderEvent>(e => Id = id);
+        }
+
+        public DataProviderFaultAggregate(Guid id, DataProvider dataProvider, Category category, string message,
+            string payload, string metadata, DateTime date, bool isJson)
+            : this(id)
+        {
+            RaiseEvent(new FaultInDataProviderEvent(id, dataProvider, category, message, payload, metadata, date, isJson));
+        }
+    }
+
+    public class DataProviderConfigurationAggregate : AggregateBase
+    {
+        private DataProviderConfigurationAggregate(Guid id)
+        {
+            Id = id;
+            Register<ConfigurationInDataProviderEvent>(e => Id = id);
+        }
+
+        public DataProviderConfigurationAggregate(Guid id, DataProvider dataProvider, Category category, string message,
+            string payload, string metadata, DateTime date, bool isJson)
+            : this(id)
+        {
+            RaiseEvent(new ConfigurationInDataProviderEvent(id, dataProvider, category, message, payload, metadata, date, isJson));
+        }
+    }
+
+    public class DataProviderSecurityAggregate : AggregateBase
+    {
+        private DataProviderSecurityAggregate(Guid id)
+        {
+            Id = id;
+            Register<SecurityInDataProviderEvent>(e => Id = id);
+        }
+
+        public DataProviderSecurityAggregate(Guid id, DataProvider dataProvider, Category category, string message,
+            string payload, string metadata, DateTime date, bool isJson)
+            : this(id)
+        {
+            RaiseEvent(new SecurityInDataProviderEvent(id, dataProvider, category, message, payload, metadata, date, isJson));
+        }
+    }
+
+    public class DataProviderTransformationAggregate : AggregateBase
+    {
+        private DataProviderTransformationAggregate(Guid id)
+        {
+            Id = id;
+            Register<TransformationInDataProviderEvent>(e => Id = id);
+        }
+
+        public DataProviderTransformationAggregate(Guid id, DataProvider dataProvider, Category category, string message,
+            string payload, string metadata, DateTime date, bool isJson)
+            : this(id)
+        {
+            RaiseEvent(new TransformationInDataProviderEvent(id, dataProvider, category, message, payload, metadata, date,
+                isJson));
+        }
+    }
+}
---
+++ app/Monitoring/source/Monitoring.Read.Denormalizer/DataProvider/DataProviderMonitoringHandler.cs
@@ -0,0 +1,115 @@
+﻿using Lace.Shared.Monitoring.Messages.Commands;
+using Monitoring.Domain.Core.Contracts;
+using Monitoring.Read.ReadModel.Models.DataProviders;
+using NServiceBus;
+
+namespace Monitoring.Read.Denormalizer.DataProvider
+{
+    public class DataProviderMonitoringHandler : IHandleMessages<DataProviderCommand>,
+        IHandleMessages<DataProviderFaultCommand>, IHandleMessages<DataProviderConfigurationCommand>,
+        IHandleMessages<DataProviderSecurityCommand>,
+        IHandleMessages<DataProviderTransformationCommand>
+    {
+        private readonly IUpdateStorage _storage;
+
+        public DataProviderMonitoringHandler(IUpdateStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public void Handle(DataProviderCommand message)
+        {
+            var @event = new DataProviderMonitoringModel(message.Id)
+            {
+                Payload = message.Message,
+                DataProviderId = (int) message.DataProvider,
+                DataProvider = message.DataProvider.ToString(),
+                Category = message.Category.ToString(),
+                CategoryId = (int) message.Category,
+                Date = message.Date,
+                RequestAggregateId = message.Id,
+                IsJson = message.IsJson,
+                Metadata = message.MetaData,
+                TimeStamp = message.Date
+            };
+
+            _storage.Add(@event);
+        }
+
+        public void Handle(DataProviderFaultCommand message)
+        {
+            var @event = new DataProviderMonitoringModel(message.Id)
+            {
+                Payload = message.Message,
+                DataProviderId = (int) message.DataProvider,
+                DataProvider = message.DataProvider.ToString(),
+                Category = message.Category.ToString(),
+                CategoryId = (int) message.Category,
+                Date = message.Date,
+                RequestAggregateId = message.Id,
+                IsJson = message.IsJson,
+                Metadata = message.MetaData,
+                TimeStamp = message.Date
+            };
+
+            _storage.Add(@event);
+        }
+
+        public void Handle(DataProviderConfigurationCommand message)
+        {
+            var @event = new DataProviderMonitoringModel(message.Id)
+            {
+                Payload = message.Message,
+                DataProviderId = (int) message.DataProvider,
+                DataProvider = message.DataProvider.ToString(),
+                Category = message.Category.ToString(),
+                CategoryId = (int) message.Category,
+                Date = message.Date,
+                RequestAggregateId = message.Id,
+                IsJson = message.IsJson,
+                Metadata = message.MetaData,
+                TimeStamp = message.Date
+            };
+
+            _storage.Add(@event);
+        }
+
+        public void Handle(DataProviderSecurityCommand message)
+        {
+            var @event = new DataProviderMonitoringModel(message.Id)
+            {
+                Payload = message.Message,
+                DataProviderId = (int) message.DataProvider,
+                DataProvider = message.DataProvider.ToString(),
+                Category = message.Category.ToString(),
+                CategoryId = (int) message.Category,
+                Date = message.Date,
+                RequestAggregateId = message.Id,
+                IsJson = message.IsJson,
+                Metadata = message.MetaData,
+                TimeStamp = message.Date
+            };
+
+            _storage.Add(@event);
+        }
+
+        public void Handle(DataProviderTransformationCommand message)
+        {
+            var @event = new DataProviderMonitoringModel(message.Id)
+            {
+                Payload = message.Message,
+                DataProviderId = (int) message.DataProvider,
+                DataProvider = message.DataProvider.ToString(),
+                Category = message.Category.ToString(),
+                CategoryId = (int) message.Category,
+                Date = message.Date,
+                RequestAggregateId = message.Id,
+                IsJson = message.IsJson,
+                Metadata = message.MetaData,
+                TimeStamp = message.Date
+            };
+
+            _storage.Add(@event);
+        }
+    }
+}
