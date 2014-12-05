c6fba7e1576c79aafa0911798dfecf52eb093ec6
Rudi Jonker <rudi@customapp.co.za>
2014-12-05 12:25:51 +0200

    LACE Unit Tests Added missing files

---
+++ app/Lace/Lace.Test.Helper/Mothers/Requests/LicensePlateNumberAllDataProvidersRequest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Lace.Domain.Core.Contracts.Requests;
+using Lace.Test.Helper.Builders.Requests;
+using Lace.Test.Helper.Mothers.Requests.Dto;
+using PackageBuilder.Domain.Entities.Packages.WriteModels;
+
+namespace Lace.Test.Helper.Mothers.Requests
+{
+    public class LicensePlateNumberAllDataProvidersRequest : Domain.Core.Requests.Contracts.ILaceRequest
+    {
+
+        public IPackage Package
+        {
+            get
+            {
+                return LicensePlateNumberAllRequestPackage.LicenseNumberPackage();
+            }
+        }
+
+        public IProvideUserInformationForRequest User
+        {
+            get
+            {
+                return new RequestUserInformation();
+            }
+        }
+
+        public IProvideContextForRequest Context
+        {
+            get
+            {
+                return new ContextInformation();
+            }
+        }
+
+        public IProvideRequestAggregation RequestAggregation
+        {
+            get
+            {
+                return new AggregationInformation();
+            }
+        }
+
+        public IProvideVehicleInformationForRequest Vehicle
+        {
+            get
+            {
+                return new RequestVehicleInformation();
+            }
+        }
+
+        public IProvideCoOrdinateInformationForRequest CoOrdinates
+        {
+            get { return new CoOrdinateInformation(); }
+        }
+
+        public IProvideJisInformation Jis
+        {
+            get { return new RequestJisInformation(); }
+        }
+
+        public DateTime RequestDate
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public string SearchTerm
+        {
+            get
+            {
+                return "XMC167GP";
+            }
+        }
+      
+    }
+}
