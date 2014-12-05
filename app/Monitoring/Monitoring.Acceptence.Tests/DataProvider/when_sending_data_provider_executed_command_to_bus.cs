99da649cc7e6568adade59e50b97f2d43cd0567b
Rudi Jonker <rudi@customapp.co.za>
2014-12-05 08:18:49 +0200

    MONITORING: Added Missing Files

---
+++ app/Monitoring/Monitoring.Acceptence.Tests/DataProvider/when_sending_data_provider_executed_command_to_bus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Lace.Shared.Monitoring.Messages.Commands;
+using Lace.Shared.Monitoring.Messages.Core;
+using Monitoring.Test.Helper.Mothers;
+using NServiceBus;
+using Xunit.Extensions;
+
+namespace Monitoring.Acceptence.Tests.DataProvider
+{
+    public class when_sending_data_provider_executed_command_to_bus : Specification
+    {
+        private readonly IBus _bus;
+        private readonly DataProviderCommand _command;
+
+
+
+        public when_sending_data_provider_executed_command_to_bus()
+        {
+            _bus = BusFactory.NServiceRabbitMqBus();
+
+            _command = new DataProviderCommand(Guid.NewGuid(),
+                Lace.Shared.Monitoring.Messages.Core.DataProvider.Audatex,
+                "Start Calling unit test", "request{}", "metadata", DateTime.UtcNow, Category.Performance, true);
+        }
+
+        public override void Observe()
+        {
+            _bus.Send(_command);
+        }
+
+        [Observation]
+        public void then_event_must_be_published_to_bus()
+        {
+            true.ShouldEqual(true);
+        }
+    }
+}
