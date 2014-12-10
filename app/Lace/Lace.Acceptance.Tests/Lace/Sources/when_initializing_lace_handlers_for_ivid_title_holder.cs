d7dd300778b549f03a562828b8bac2dbd7a88431
Rudi Jonker <rudi@customapp.co.za>
2014-12-10 16:35:48 +0200

    LACE: Unit Testing fixes

--- app/Lace/Lace.Acceptance.Tests/Lace/Sources/when_initializing_lace_handlers_for_ivid_title_holder.cs
+++ app/Lace/Lace.Acceptance.Tests/Lace/Sources/when_initializing_lace_handlers_for_ivid_title_holder.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+using Lace.Domain.Core.Contracts;
 using Lace.Domain.Core.Requests.Contracts;
-using Lace.Domain.Infrastructure.Core.Contracts;
-using Lace.Domain.Infrastructure.Core.Dto;
-using Lace.Domain.Infrastructure.EntryPoint;
-using Lace.Domain.Infrastructure.EntryPoint.Builder.Factory;
+using Lace.Domain.DataProviders.Core.Contracts;
+using Lace.Domain.DataProviders.IvidTitleHolder;
 using Lace.Shared.Monitoring.Messages.Shared;
 using Lace.Test.Helper.Builders.Buses;
 using Lace.Test.Helper.Builders.Requests;
+using Lace.Test.Helper.Builders.Responses;
 using Xunit.Extensions;
 
 namespace Lace.Acceptance.Tests.Lace.Sources
@@ -16,37 +15,34 @@ namespace Lace.Acceptance.Tests.Lace.Sources
     {
 
         private readonly ILaceRequest _request;
+        private readonly IProvideResponseFromLaceDataProviders _response;
         private readonly ISendMonitoringMessages _monitoring;
-        private readonly IBootstrap _initialize;
-        private IList<LaceExternalSourceResponse> _laceResponses;
-        private readonly IBuildSourceChain _buildSourceChain;
+        private readonly IExecuteTheDataProviderSource _dataProvider;
 
         public when_initializing_lace_handlers_for_ivid_title_holder()
         {
+
             _monitoring = BusBuilder.ForMonitoringMessages(Guid.NewGuid());
             _request = new LicensePlateRequestBuilder().ForIvidTitleHolder();
-            _buildSourceChain = new CreateSourceChain(_request.Package);
-            _buildSourceChain.Build();
-            _initialize = new Initialize(new LaceResponse(), _request, _monitoring, _buildSourceChain);
+            _response = new LaceResponseBuilder().WithIvidResponseHandled();
+            _dataProvider = new IvidTitleHolderDataProvider(_request, null, null);
         }
 
         public override void Observe()
         {
-            _initialize.Execute();
-            _laceResponses = _initialize.LaceResponses;
+            _dataProvider.CallSource(_response, _monitoring);
         }
 
         [Observation]
-        public void lace_functional_test_ivid_title_holder_response_should_be_handled_test()
+        public void lace_ivid_title_holder_response_should_be_handled_test()
         {
-            _laceResponses[0].Response.IvidTitleHolderResponseHandled.Handled.ShouldBeTrue();
+            _response.IvidTitleHolderResponseHandled.Handled.ShouldBeTrue();
         }
 
         [Observation]
-        public void lace_functional_test_ivid_title_holder_response_shuould_not_be_null_test()
+        public void lace_ivid_title_holder_response_shuould_not_be_null_test()
         {
-            _laceResponses[0].Response.IvidTitleHolderResponse.ShouldNotBeNull();
+            _response.IvidTitleHolderResponse.ShouldNotBeNull();
         }
-
     }
 }
