605c5f598551cd001eb6f501cbfc64ab8e0cb819
Rudi Jonker <rudi@customapp.co.za>
2014-12-12 12:02:27 +0200

    Unit Tests Lace

--- app/Lace/Source/Lace.Domain.DataProviders.Lightstone/Core/Models/Metric.cs
+++ app/Lace/Source/Lace.Domain.DataProviders.Lightstone/Core/Models/Metric.cs
@@ -7,10 +7,11 @@
             
         }
 
-        public Metric(int metricId, string metricName, int aggregationId, int hasBand, int hasMake, int hasCarType, int hasCar, int hasYear, int hasMuncipality)
+        public Metric(int metricId, string metricName, int dataTypeId, int aggregationId, int hasBand, int hasMake, int hasCarType, int hasCar, int hasYear, int hasMuncipality)
         {
             Metric_ID = metricId;
             MetricName = metricName;
+            DataType_ID = dataTypeId;
             Aggregation_ID = aggregationId;
             HasBand = hasBand;
             HasMake = hasMake;
@@ -21,6 +22,7 @@
         }
 
         public int Metric_ID { get; set; }
+        public int DataType_ID { get; set; }
         public string MetricName { get; set; }
         public int Aggregation_ID { get; set; }
         public int HasBand { get; set; }
