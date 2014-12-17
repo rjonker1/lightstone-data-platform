94fdba34174dc9f6b9e0a70f8b7cd4448748efc6
Rudi Jonker <rudi@customapp.co.za>
2014-12-17 12:35:06 +0200

    LACE - Monitoring Messages

---
+++ app/Lace/Source/Lace.Shared.Messages/Commands/DataProviderHasExecutedCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lace.Shared.Monitoring.Messages.Commands
+{
+    [Serializable]
+    public class DataProviderHasExecutedCommand
+    {
+        public DataProviderCommandDto DataProviderCommand { get; private set; }
+
+        public DataProviderHasExecutedCommand(DataProviderCommandDto command)
+        {
+            DataProviderCommand = command;
+        }
+    }
+}
---
+++ app/Lace/Source/Lace.Shared.Messages/Commands/DataProviderWasCalledCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Lace.Shared.Monitoring.Messages.Commands
+{
+    [Serializable]
+    public class DataProviderWasCalledCommand
+    {
+        public DataProviderCommandDto DataProviderCommand { get; private set; }
+
+        public DataProviderWasCalledCommand(DataProviderCommandDto command)
+        {
+            DataProviderCommand = command;
+        }
+    }
+}
