c4279018dfe7b5184410bd5f97f933fe61d4c68b
Allistaire Clark <allistairec@lightstone.co.za>
2014-12-22 14:37:28 +0200

    PackageBuilder Industries E2E unit tests

---
+++ app/PackageBuilder/PackageBuilder.Web.UI.Tests/Industries/IndustriesPageObjects.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using Protractor;
+
+namespace PackageBuilder.Web.UI.Tests.Industries
+{
+    class IndustriesPageObjects
+    {
+
+        public NgWebDriver ngDriver;
+
+        public IndustriesPageObjects(IWebDriver driver, string url)
+        {
+            ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl(url);
+        }
+        
+        public string FindByIdWithAttribute(string elementId, string attribute)
+        {
+            return ngDriver.FindElement(By.Id(elementId)).GetAttribute(attribute);
+        }
+        
+        public int GetIndustriesCount()
+        {
+            //Return number of industries in ui.Grid
+            return ngDriver.FindElements(NgBy.Repeater("(rowRenderIndex, row) in rowContainer.renderedRows track by row.uid")).Count;
+        }
+
+
+    }
+}
---
+++ app/PackageBuilder/PackageBuilder.Web.UI.Tests/Industries/when_loading_industries.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+
+namespace PackageBuilder.Web.UI.Tests.Industries
+{
+    class when_loading_industries
+    {
+        Common common = new Common();
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            driver = new PhantomJSDriver(common.DRIVER_LOC);
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(30));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void should_set_title()
+        {
+
+            var industriesPage = new IndustriesPageObjects(driver, common.CONN_STRING + "#/industries");
+
+            Assert.AreEqual("Industries", industriesPage.FindByIdWithAttribute("title", "title"));
+        }
+
+        [Test]
+        public void should_have_one_or_more_industries()
+        {
+            var industriesPage = new IndustriesPageObjects(driver, common.CONN_STRING + "#/industries");
+
+            Assert.GreaterOrEqual(1, industriesPage.GetIndustriesCount());
+        }
+
+    }
+}
