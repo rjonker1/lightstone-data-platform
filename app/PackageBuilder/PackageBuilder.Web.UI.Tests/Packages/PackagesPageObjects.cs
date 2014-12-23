37c0b38a56bc37da98e73229c2961510cc2c029f
Allistaire Clark <allistairec@lightstone.co.za>
2014-12-23 16:27:42 +0200

    PackageBuilder Packages E2E unit tests (BASE)

---
+++ app/PackageBuilder/PackageBuilder.Web.UI.Tests/Packages/PackagesPageObjects.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Protractor;
+
+namespace PackageBuilder.Web.UI.Tests.Packages
+{
+    class PackagesPageObjects
+    {
+
+        public NgWebDriver ngDriver;
+
+        //ngDriver setup
+        public PackagesPageObjects(IWebDriver driver, string url)
+        {
+            ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl(url);
+        }
+
+        public string FindByIdWithAttribute(string elementId, string attribute)
+        {
+            return ngDriver.FindElement(By.Id(elementId)).GetAttribute(attribute);
+        }
+
+        public int GetAvailableDataProviders(string query)
+        {
+            //Return number of dataProviders in ui.Grid
+            return ngDriver.FindElements(NgBy.Repeater(query)).Count;
+        }
+
+        public void selectDropDownByNum(int optionNumber)
+        {
+            
+            if (optionNumber >= 0)
+            {
+                var options = ngDriver.FindElements(By.TagName("option"));
+                options[optionNumber].Click();
+            }
+            
+        }
+
+    }
+}
---
+++ app/PackageBuilder/PackageBuilder.Web.UI.Tests/Packages/when_loading_package_create_page.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using Protractor;
+
+namespace PackageBuilder.Web.UI.Tests.Packages
+{
+    class when_loading_package_create_page
+    {
+
+        private Common.Common common = new Common.Common();
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            driver = new PhantomJSDriver(common.DRIVER_LOC);
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(30));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        //TODO: Need to implement a mock to edit specific dataprovider /{id}/{version}
+        [Test]
+        public void should_set_title()
+        {
+
+            var dataProvidersPage = new PackagesPageObjects(driver, common.CONN_STRING + "#/package-maintenance");
+
+            Assert.AreEqual("Package Maintenance - Create", dataProvidersPage.FindByIdWithAttribute("title", "title"));
+        }
+        
+        [Test]
+        public void should_set_states_options_for_package()
+        {
+
+            var dataProvidersPage = new PackagesPageObjects(driver, common.CONN_STRING + "#/package-maintenance");
+
+            dataProvidersPage.selectDropDownByNum(1);
+
+            var options = dataProvidersPage.ngDriver.FindElements(By.TagName("option"));
+
+            foreach (NgWebElement opt in options)
+            {
+                if (opt.Selected)
+                {
+                    Assert.AreEqual("Expired", opt.Text);
+                }
+            }
+        }
+        
+        [Test]
+        public void should_have_dataproviders_available()
+        {
+
+            var dataProvidersPage = new PackagesPageObjects(driver, common.CONN_STRING + "#/package-maintenance");
+
+            Assert.GreaterOrEqual(dataProvidersPage.GetAvailableDataProviders("provider in providers"), 1);
+        }
+
+    }
+}
