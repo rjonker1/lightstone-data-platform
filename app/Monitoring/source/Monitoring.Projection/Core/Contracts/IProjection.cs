09b82c944b8f9e2b140bdb2c438dfb3dbb95a204
Rudi Jonker <rudi@customapp.co.za>
2014-12-24 13:45:57 +0200

    Monitoring Projections

--- app/Monitoring/source/Monitoring.Projection/Core/Contracts/IProjection.cs
+++ app/Monitoring/source/Monitoring.Projection/Core/Contracts/IProjection.cs
@@ -1,6 +1,4 @@
-﻿using Monitoring.Domain.Core.Contracts;
-
-namespace Monitoring.Projection.Core.Contracts
+﻿namespace Monitoring.Projection.Core.Contracts
 {
     public interface IProjection
     {
