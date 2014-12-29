fdf990f000c14c40049f397af9a71e868a310ed0
Allistaire Clark <allistairec@lightstone.co.za>
2014-12-29 12:30:58 +0200

    UserManagement.Api Windsor/NHibernate implented

---
+++ app/UserManagement/UserManagement.Domain.Core/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace UserManagement.Domain.Core.Entities
+{
+    [DataContract]
+    public abstract class Entity : IEntity
+    {
+        [DataMember]
+        public virtual Guid Id { get; protected set; }
+
+        protected Entity() { }
+
+        protected Entity(Guid id)
+        {
+            Id = id == new Guid() ? Guid.NewGuid() : id;
+        }
+    }
+}
---
+++ app/UserManagement/UserManagement.Infrastructure/NHibernate/Conventions/DomainSignatureConvention.cs
@@ -0,0 +1,14 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+using UserManagement.Domain.Core.NHibernate.Attributes;
+
+namespace UserManagement.Infrastructure.NHibernate.Conventions
+{
+    public class DomainSignatureConvention : AttributePropertyConvention<DomainSignatureAttribute>
+    {
+        protected override void Apply(DomainSignatureAttribute attribute, IPropertyInstance instance)
+        {
+            instance.UniqueKey(instance.EntityType.Name + "DomainSignature");
+        }
+    }
+}
