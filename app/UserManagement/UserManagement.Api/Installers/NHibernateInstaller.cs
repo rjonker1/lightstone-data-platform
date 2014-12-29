fb855dffc752d1b7930e9d74d6f1b6585417162e
Allistaire Clark <allistairec@lightstone.co.za>
2014-12-29 13:19:32 +0200

    User model

--- app/UserManagement/UserManagement.Api/Installers/NHibernateInstaller.cs
+++ app/UserManagement/UserManagement.Api/Installers/NHibernateInstaller.cs
@@ -3,10 +3,12 @@ using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using FluentNHibernate.Automapping;
 using FluentNHibernate.Cfg;
+using FluentNHibernate.Conventions.Helpers;
 using NHibernate;
 using NHibernate.Cfg;
 using UserManagement.Domain.Core.Entities;
 using UserManagement.Domain.Entities;
+using UserManagement.Infrastructure.NHibernate.Conventions;
 
 namespace UserManagement.Api.Installers
 {
@@ -17,15 +19,18 @@ namespace UserManagement.Api.Installers
 
             var configure = new Configuration().Configure();
             container.Register(Component.For<Configuration>().UsingFactoryMethod(() => Fluently.Configure(configure)
-                .Mappings(cfg =>
-                {
-                    cfg.AutoMappings
-                        .Add(AutoMap.AssemblyOf<User>()
-                            .Where(type => type.IsSubclassOf(typeof(Entity)))
-                            .IgnoreBase<Entity>()
-                            );
-                    
-                }).BuildConfiguration()));
+                .Mappings(cfg => cfg.AutoMappings
+
+                    .Add(AutoMap.AssemblyOf<User>()
+                        .Where(type => type.IsSubclassOf(typeof(Entity)))
+                        .IgnoreBase<Entity>()
+                        .Conventions.Add
+                        (
+                            ConventionBuilder.Id.Always(x => x.GeneratedBy.Assigned()),
+                            ForeignKey.EndsWith("Id"),
+                            new DomainSignatureConvention()
+                        )
+                        )).BuildConfiguration()));
 
             container.Register(Component.For<ISessionFactory>()
                      .UsingFactoryMethod(kernal => container.Resolve<Configuration>().BuildSessionFactory())
