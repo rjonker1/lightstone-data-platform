101d3df7671cd372fe47428be1625d3626070462
Allistaire Clark <allistairec@lightstone.co.za>
2014-12-29 16:44:36 +0200

    Customer model

---
+++ app/UserManagement/UserManagement.Domain.Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UserManagement.Domain.Core.Entities;
+
+namespace UserManagement.Domain.Entities
+{
+    public class Customer : Entity
+    {
+
+        public virtual Guid Id { get; protected internal set; }
+        public virtual string CustomerName { get; protected internal set; }
+        public virtual Guid AccountOwnerId { get; protected internal set; }
+        public virtual Guid CustomerProfileId { get; protected internal set; }
+        public virtual Guid ProvinceId { get; protected internal set; }
+
+    }
+}
