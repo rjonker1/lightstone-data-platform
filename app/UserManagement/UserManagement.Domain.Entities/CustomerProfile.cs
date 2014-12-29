878d2cf280bcd5f93b38f6c1457aa241f3b051da
Allistaire Clark <allistairec@lightstone.co.za>
2014-12-29 16:33:50 +0200

    CustomerProfile model

---
+++ app/UserManagement/UserManagement.Domain.Entities/CustomerProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UserManagement.Domain.Core.Entities;
+
+namespace UserManagement.Domain.Entities
+{
+    public class CustomerProfile : Entity
+    {
+
+        public virtual Guid Id { get; protected internal set; }
+        public virtual Guid CommercialStateId { get; protected internal set; }
+        public virtual Guid CreateSourceId { get; protected internal set; }
+        public virtual DateTime FirstCreateDate { get; protected internal set; }
+        public virtual string LastUpdateBy { get; protected internal set; }
+        public virtual DateTime LastUpdateDate { get; protected internal set; }
+        public virtual Guid PlatformStatusId { get; protected internal set; }
+        public virtual Guid BillingId { get; protected internal set; }
+        public virtual string PastelId { get; protected internal set; }
+
+    }
+}
