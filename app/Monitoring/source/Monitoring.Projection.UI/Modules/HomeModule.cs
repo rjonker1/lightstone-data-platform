c66bf8cd577df1406b9e7592ea87a5e11db3a961
Rudi Jonker <rudi@customapp.co.za>
2015-01-06 10:21:49 +0200

    Monitoring - Projection UI

---
+++ app/Monitoring/source/Monitoring.Projection.UI/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using Monitoring.Read.ReadModel.Models.DataProviders;
+using Nancy;
+
+namespace Monitoring.Projection.UI.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {
+                var model = new MonitoringDataProviderModel(Guid.NewGuid());
+                return View["Index", model];
+            };
+        }
+    }
+}
