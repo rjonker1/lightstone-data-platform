5270a32be3694dda10c1ef3254c2cff076bfb2e2
Rudi Jonker <rudi@customapp.co.za>
2015-01-06 10:46:33 +0200

    Shared - RabbitMQ Bus Factory

---
+++ app/Shared/Shared/Messaging/Events/IMonitorEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DataPlatform.Shared.Messaging.Events
+{
+    interface IMonitorEvent : IPublishableMessage
+    {
+        Guid AggregateId { get; }
+        DateTime Date { get; }
+        string Payload { get; }
+    }
+
+    [Serializable]
+    public class MonitoringEvent : IMonitorEvent
+    {
+        public Guid AggregateId { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Payload { get; private set; }
+
+        public MonitoringEvent()
+        {
+
+        }
+
+        public MonitoringEvent(Guid aggregateId, string payload, DateTime date)
+        {
+            AggregateId = aggregateId;
+            Payload = payload;
+            Date = date;
+        }
+    }
+}
---
+++ app/Shared/Shared/RabbitMQ/BusFactorycs.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus;
+using NServiceBus.Features;
+
+namespace DataPlatform.Shared.RabbitMQ
+{
+    internal class BusFactory
+    {
+        public static IBus CreateBus(string commandNamespace)
+        {
+            var configuration = new BusConfiguration();
+
+            configuration.UseTransport<RabbitMQTransport>();
+            configuration.DisableFeature<TimeoutManager>();
+            configuration.Conventions()
+                .DefiningCommandsAs(
+                    c => c.Namespace != null && c.Namespace.StartsWith(commandNamespace));
+            var bus = Bus.Create(configuration);
+            return bus;
+        }
+    }
+}
