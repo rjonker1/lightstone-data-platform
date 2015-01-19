bf1c8a46fe8c76ac96137109f18ecc64b657a385
Allistaire Clark <allistairec@lightstone.co.za>
2015-01-19 14:59:48 +0200

    Meta for OData

--- app/UserManagement/UserManagement.Api/Modules/IndexModule.cs
+++ app/UserManagement/UserManagement.Api/Modules/IndexModule.cs
@@ -1,15 +1,28 @@
-﻿using Nancy;
+﻿using System.Collections.Generic;
+using Nancy;
+using Nancy.OData;
 
 namespace UserManagement.Api.Modules
 {
     public class IndexModule : NancyModule
     {
         public IndexModule()
+            : base("/")
         {
-            Get["/"] = parameters =>
+            Get["/Test/{stuff}/"] = x =>
             {
-                return "Hello World";
+                return Response.AsOData(new List<Stuff> {
+                new Stuff {Id = 1, Name = "one" }, 
+                     new Stuff {Id = 2, Name = "two" }, 
+                        new Stuff {Id = 3, Name = "Three"}
+                });
             };
         }
+    }
+
+    public class Stuff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
