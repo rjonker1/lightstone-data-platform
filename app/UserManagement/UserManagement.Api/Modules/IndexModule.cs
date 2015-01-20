2129ae776d0e7ac95183c6d82e7b773961867fe8
Allistaire Clark <allistairec@lightstone.co.za>
2015-01-20 12:11:13 +0200

    Meta Cleanup

--- app/UserManagement/UserManagement.Api/Modules/IndexModule.cs
+++ app/UserManagement/UserManagement.Api/Modules/IndexModule.cs
@@ -1,28 +1,13 @@
 ﻿using System.Collections.Generic;
 using Nancy;
-using Nancy.OData;
 
 namespace UserManagement.Api.Modules
 {
     public class IndexModule : NancyModule
     {
         public IndexModule()
-            : base("/")
         {
-            Get["/Test/{stuff}/"] = x =>
-            {
-                return Response.AsOData(new List<Stuff> {
-                new Stuff {Id = 1, Name = "one" }, 
-                     new Stuff {Id = 2, Name = "two" }, 
-                        new Stuff {Id = 3, Name = "Three"}
-                });
-            };
+            Get["/"] = x => "Hello World";
         }
-    }
-
-    public class Stuff
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
     }
 }
