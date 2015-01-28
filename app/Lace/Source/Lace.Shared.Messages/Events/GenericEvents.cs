7bc1e0080a48244d737d3b4770821bba1f92e92a
Rudi Jonker <rudi@customapp.co.za>
2015-01-28 17:51:45 +0200

    Monitoring Lace added missing files

---
+++ app/Lace/Source/Lace.Shared.Messages/Events/GenericEvents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.Serialization;
+using DataPlatform.Shared.Enums;
+using Lace.Shared.Monitoring.Messages.Core;
+
+namespace Lace.Shared.Monitoring.Messages.Events
+{
+    [Serializable]
+    [DataContract]
+    public class ErrorThrown : IEvent
+    {
+        public ErrorThrown(Guid id, DataProviderCommandSource dataProvider, string message,
+            object payload,
+            object metadata, DateTime date, Category category)
+        {
+            Id = id;
+            DataProviderCommandSource = dataProvider;
+            Message = message;
+            Payload = payload;
+            MetaData = metadata;
+            Payload = payload;
+            Category = category;
+            Date = date;
+        }
+
+        [DataMember]
+        public Category Category { get; private set; }
+
+        [DataMember]
+        public DataProviderCommandSource DataProviderCommandSource { get; private set; }
+
+        [DataMember]
+        public DateTime Date { get; private set; }
+
+        [DataMember]
+        public Guid Id { get; private set; }
+
+        [DataMember]
+        public string Message { get; private set; }
+
+        [DataMember]
+        public object MetaData { get; private set; }
+
+        [DataMember]
+        public object Payload { get; private set; }
+    }
+}
---
+++ app/Monitoring/source/Monitoring.Read.Denormalizer/DataProviders/IvidMonitoringHandler.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using Lace.Shared.Extensions;
+using Lace.Shared.Monitoring.Messages.Events;
+using Monitoring.Domain.Core.Contracts;
+using Monitoring.Read.ReadModel.Models;
+using NServiceBus;
+
+namespace Monitoring.Read.Denormalizer.DataProviders
+{
+    public class IvidMonitoringHandler : IHandleMessages<IvidExecutionHasStarted>, IHandleMessages<IvidExcutionHasEnded>,
+        IHandleMessages<IvidDataSourceCallHasStarted>, IHandleMessages<IvidDataSourceCallHasEnded>,
+        IHandleMessages<IvidSecurityFlag>, IHandleMessages<IvidConfigured>, IHandleMessages<IvidResponseTransformed>
+    {
+
+        private readonly IAccessToStorage _storage;
+
+        public IvidMonitoringHandler(IAccessToStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public void Handle(IvidExecutionHasStarted message)
+        {
+            var model = new MonitoringStorageModel(message.Id)
+            {
+                Payload = Encoding.UTF8.GetBytes(message.ObjectToJson()),
+                Source = (int) message.DataProviderCommandSource,
+                Date = message.Date,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            _storage.Add(model);
+        }
+
+        public void Handle(IvidExcutionHasEnded message)
+        {
+            var model = new MonitoringStorageModel(message.Id)
+            {
+                Payload = Encoding.UTF8.GetBytes(message.ObjectToJson()),
+                Source = (int) message.DataProviderCommandSource,
+                Date = message.Date,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            _storage.Add(model);
+        }
+
+        public void Handle(IvidResponseTransformed message)
+        {
+            var model = new MonitoringStorageModel(message.Id)
+            {
+                Payload = Encoding.UTF8.GetBytes(message.ObjectToJson()),
+                Source = (int) message.DataProviderCommandSource,
+                Date = message.Date,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            _storage.Add(model);
+        }
+
+        public void Handle(IvidConfigured message)
+        {
+            var model = new MonitoringStorageModel(message.Id)
+            {
+                Payload = Encoding.UTF8.GetBytes(message.ObjectToJson()),
+                Source = (int) message.DataProviderCommandSource,
+                Date = message.Date,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            _storage.Add(model);
+        }
+
+        public void Handle(IvidSecurityFlag message)
+        {
+            var model = new MonitoringStorageModel(message.Id)
+            {
+                Payload = Encoding.UTF8.GetBytes(message.ObjectToJson()),
+                Source = (int) message.DataProviderCommandSource,
+                Date = message.Date,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            _storage.Add(model);
+        }
+
+        public void Handle(IvidDataSourceCallHasEnded message)
+        {
+            var model = new MonitoringStorageModel(message.Id)
+            {
+                Payload = Encoding.UTF8.GetBytes(message.ObjectToJson()),
+                Source = (int) message.DataProviderCommandSource,
+                Date = message.Date,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            _storage.Add(model);
+        }
+
+        public void Handle(IvidDataSourceCallHasStarted message)
+        {
+            var model = new MonitoringStorageModel(message.Id)
+            {
+                Payload = Encoding.UTF8.GetBytes(message.ObjectToJson()),
+                Source = (int) message.DataProviderCommandSource,
+                Date = message.Date,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            _storage.Add(model);
+        }
+    }
+}
