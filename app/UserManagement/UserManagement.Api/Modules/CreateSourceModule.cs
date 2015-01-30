e14147a0dbc4e06fbb2b511eb9786f54884a42d0
Allistaire Clark <allistairec@lightstone.co.za>
2015-01-30 17:00:10 +0200

    User Management CreateSource Module

---
+++ app/UserManagement/UserManagement.Api/Modules/CreateSourceModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using UserManagement.Domain.Core.Repositories;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Api.Modules
+{
+    public class CreateSourceModule : NancyModule
+    {
+        public CreateSourceModule(IRepository<CreateSource> createSources)
+        {
+
+            Get["/CreateSources"] = _ => Response.AsJson(createSources);
+        }
+    }
+}
