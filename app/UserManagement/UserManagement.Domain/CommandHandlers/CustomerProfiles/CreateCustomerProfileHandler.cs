7e7fd9ceb48f4636e95c3aac2baaa07cc17a6be5
Allistaire Clark <allistairec@lightstone.co.za>
2015-01-30 12:19:51 +0200

    User Management CustomerProfile Command | Handler setup

---
+++ app/UserManagement/UserManagement.Domain/CommandHandlers/CustomerProfiles/CreateCustomerProfileHandler.cs
@@ -0,0 +1,25 @@
+﻿using UserManagement.Domain.Core.MessageHandling;
+using UserManagement.Domain.Core.Repositories;
+using UserManagement.Domain.Entities;
+using UserManagement.Domain.Entities.Commands.CustomerProfiles;
+
+namespace UserManagement.Domain.CommandHandlers.CustomerProfiles
+{
+    public class CreateCustomerProfileHandler : AbstractMessageHandler<CreateCustomerProfile>
+    {
+
+        private readonly IRepository<CustomerProfile> _repository;
+
+        public CreateCustomerProfileHandler(IRepository<CustomerProfile> repository)
+        {
+            _repository = repository;
+        }
+
+        public override void Handle(CreateCustomerProfile command)
+        {
+
+            _repository.Save(new CustomerProfile(command.Id, command.FirstCreateDate, command.LastUpdateBy, command.LastUpdateDate, command.PastelID, command.Billing, command.CommercialState,
+                                                    command.CreateSource, command.Customer, command.PlatformStatus/*, command.ProfileDetail*/));
+        }
+    }
+}
