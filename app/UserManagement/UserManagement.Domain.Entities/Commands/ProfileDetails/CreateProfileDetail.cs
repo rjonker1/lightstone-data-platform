78beee82968b68b48654e35321d9bf64b5cbd3bb
Allistaire Clark <allistairec@lightstone.co.za>
2015-01-30 14:12:40 +0200

    User Management ProfileDetail Command | Handler

---
+++ app/UserManagement/UserManagement.Domain.Entities/Commands/ProfileDetails/CreateProfileDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UserManagement.Domain.Core.Commands;
+
+namespace UserManagement.Domain.Entities.Commands.ProfileDetails
+{
+    public class CreateProfileDetail : DomainCommand
+    {
+
+        public string LegalEntityName;
+        public string AccountsContactName;
+        public string EmailAddress;
+        public string TelephoneNumber;
+        public string VATNumber;
+
+        public Address Address;
+        public Address Address1;
+
+        public CreateProfileDetail(string legalEntityName, string accountsContactName, string emailAddress, string telephoneNumber, string vatNumber, Address address, Address address1)
+        {
+            Id = Guid.NewGuid();
+            LegalEntityName = legalEntityName;
+            AccountsContactName = accountsContactName;
+            EmailAddress = emailAddress;
+            TelephoneNumber = telephoneNumber;
+            VATNumber = vatNumber;
+            Address = address;
+            Address1 = address1;
+        }
+    }
+}
---
+++ app/UserManagement/UserManagement.Domain/CommandHandlers/ProfileDetails/CreateProfileDetailHandler.cs
@@ -0,0 +1,25 @@
+﻿using UserManagement.Domain.Core.MessageHandling;
+using UserManagement.Domain.Core.Repositories;
+using UserManagement.Domain.Entities;
+using UserManagement.Domain.Entities.Commands.ProfileDetails;
+
+namespace UserManagement.Domain.CommandHandlers.ProfileDetails
+{
+    public class CreateProfileDetailHandler : AbstractMessageHandler<CreateProfileDetail>
+    {
+
+        private readonly IRepository<ProfileDetail> _repository;
+
+        public CreateProfileDetailHandler(IRepository<ProfileDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public override void Handle(CreateProfileDetail command)
+        {
+
+            _repository.Save(new ProfileDetail(command.Id, command.LegalEntityName, command.AccountsContactName, command.EmailAddress, command.TelephoneNumber, command.VATNumber, 
+                                                command.Address, command.Address1));
+        }
+    }
+}
