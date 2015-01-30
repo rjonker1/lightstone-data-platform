c4d0c68dd3d6c6f8edb33a84400e6c3871f9bb89
Allistaire Clark <allistairec@lightstone.co.za>
2015-01-30 12:56:09 +0200

    User Management ProfileDetail Model

--- app/UserManagement/UserManagement.Domain.Entities/ProfileDetail.cs
+++ app/UserManagement/UserManagement.Domain.Entities/ProfileDetail.cs
@@ -10,22 +10,28 @@ namespace UserManagement.Domain.Entities
         public virtual string LegalEntityName { get; set; }
         public virtual string AccountsContactName { get; set; }
         public virtual string EmailAddress { get; set; }
-        public virtual Guid? PhysicalAddressId { get; set; }
-        public virtual Guid? PostalAddressId { get; set; }
         public virtual string TelephoneNumber { get; set; }
         public virtual string VATNumber { get; set; }
 
         public virtual Address Address { get; set; }
         public virtual Address Address1 { get; set; }
-        public virtual ICollection<Client> Client { get; set; }
-        public virtual ICollection<Contract> Contract { get; set; }
-        public virtual ICollection<CustomerProfile> CustomerProfile { get; set; }
+        //public virtual ICollection<Client> Client { get; set; }
+        //public virtual ICollection<Contract> Contract { get; set; }
+        //public virtual ICollection<CustomerProfile> CustomerProfile { get; set; }
 
-        public ProfileDetail()
+        protected ProfileDetail() { }
+
+        public ProfileDetail(Guid id, string legalEntityName, string accountsContactName, string emailAddress, string telephoneNumber, string vatNumber, Address address, Address address1)
+            : base(id)
         {
-            Client = new HashSet<Client>();
-            Contract = new HashSet<Contract>();
-            CustomerProfile = new HashSet<CustomerProfile>();
+
+            LegalEntityName = legalEntityName;
+            AccountsContactName = accountsContactName;
+            EmailAddress = emailAddress;
+            TelephoneNumber = telephoneNumber;
+            VATNumber = vatNumber;
+            Address = address;
+            Address1 = address1;
         }
     }
 }
