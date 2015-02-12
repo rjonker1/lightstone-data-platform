afb87b64d6e4a0ba8e51faee972fce0637732df3
Rudi Jonker <rudi@customapp.co.za>
2015-02-12 17:32:00 +0200

    API Unit/Acceptance tests

--- app/Api/Api.Unit.Tests/when_sending_drivers_license_request.cs
+++ app/Api/Api.Unit.Tests/when_sending_drivers_license_request.cs
@@ -1,4 +1,5 @@
 ﻿using Api.Domain.Verification.Infrastructure.Dto;
+using Api.Modules.Verification;
 using Api.Tests.Helper.Builder;
 using Api.Unit.Tests.Fakes;
 using Nancy;
@@ -9,15 +10,16 @@ namespace Api.Unit.Tests
 {
     public class when_sending_drivers_license_request : Specification
     {
-        //private readonly DefaultNancyBootstrapper _bootstrapper;
+        private readonly DefaultNancyBootstrapper _bootstrapper;
         private readonly Browser _browser;
         private BrowserResponse _result;
         private readonly DriversLicenseRequestDto _request;
 
         public when_sending_drivers_license_request()
         {
-            //_bootstrapper = new TestBootstrapper("user");
-            _browser = new Browser(new TestBootstrapper("user"));
+            _bootstrapper = new Bootstrapper();
+            _browser = new Browser(_bootstrapper, defaults: to => to.Accept("application/json"));
+            //_browser = new Browser(with => with.Module<DriversLicenseModule>());
             _request = DriversLicenseScanBuilder.ForDriversLicenseRequest();
         }
 
@@ -37,15 +39,14 @@ namespace Api.Unit.Tests
         [Observation]
         public void then_drivers_verifcation_should_return_a_valid_result()
         {
-            _result = _browser.Post("/api/verification/driversLicense", with =>
+            var result = _browser.Post("/api/verification/driversLicense/EB49A837-D9E3-4F2A-8DC9-2CB0BB5D48E2/1", with =>
             {
+                with.HttpRequest();
                 with.Header("Authorization", "ApiKey 4E7106BA-16B6-44F2-AF4C-D1C411440F8E");
-                with.Header("Accept", "application/json");
-                with.Query("packageId", "EB49A837-D9E3-4F2A-8DC9-2CB0BB5D48E2");
-                with.Query("packageVersion", "1");
+                with.Header("Content-Type", "application/json");
                 with.JsonBody(_request);
             });
-            _result.ShouldNotBeNull();
+            result.ShouldNotBeNull();
         }
     }
 
