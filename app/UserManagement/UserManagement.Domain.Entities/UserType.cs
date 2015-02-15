3ff57070ff2924e43365a636372ea722927559f8
Wayne Munro <waynemunro@hotmail.com>
2015-02-15 16:25:44 +0200

    UserType temp refactor

--- app/UserManagement/UserManagement.Domain.Entities/UserType.cs
+++ app/UserManagement/UserManagement.Domain.Entities/UserType.cs
@@ -13,9 +13,16 @@ namespace UserManagement.Domain.Entities
             Value = name;
         }
 
-        //public override string ToString()
-        //{
-        //    return "{0} - {1} - {2}".FormatWith(Id, Name, GetType());
-        //}
+        public override string ToString()
+        {
+            // Had to comment this out and replace implementation as the common packack was not \
+            // being referenced / installed for some reason. Hope the resutl is the same.
+
+            // TODO: revert the implementaion.
+            
+           // return "{0} - {1} - {2}".FormatWith(Id, Name, GetType());
+
+            return string.Format("{0} - {1} - {2}", Id, Name, GetType());
+        }
     }
 }
