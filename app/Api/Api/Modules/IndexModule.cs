00b25ada36d96c474b273f6f75b6c9e190a04c00
Rudi Jonker <rudi@customapp.co.za>
2015-02-16 16:14:33 +0200

    API, UM API, user auth API and Package Builder API

--- app/Api/Api/Modules/IndexModule.cs
+++ app/Api/Api/Modules/IndexModule.cs
@@ -14,13 +14,12 @@ using PackageBuilder.Core.Entities;
 using PackageBuilder.Domain.Entities.Packages.WriteModels;
 using Shared.BuildingBlocks.Api;
 using Shared.BuildingBlocks.Api.Security;
-using Package = DataPlatform.Shared.Dtos.Package;
 
 namespace Api.Modules
 {
     public class IndexModule : SecureModule
     {
-        public IndexModule(IPbApiClient packageBuilderApi, IEntryPoint entryPoint, IConnectToBilling billingConnector, IUmApiAuthenticator userManagementApi)
+        public IndexModule(IPackageBuilderApiClient packageBuilderApi, IEntryPoint entryPoint, IConnectToBilling billingConnector, IUserManagementApiClient userManagementApi)
         {
             Get["/"] = parameters =>
             {
@@ -39,8 +38,11 @@ namespace Api.Modules
 
                 var apiRequest = this.Bind<ApiRequestDto>();
 
-                var packageResponse = packageBuilderApi.Get<Package>(token, "package/" + parameters.action);
+                //get package id from user management
+                var packageDetails = userManagementApi.Post<ContractResponse>(token, "/Contracts/GetPackage",
+                    new {ContractId = apiRequest.ContractId});
 
+                var packageResponse = packageBuilderApi.Get(token, "Packages/Package/" + packageDetails.Package.Id);
 
                 var package = Mapper.DynamicMap<IPackage>(packageResponse);
                 var vehicle = this.Bind<Vehicle>();
@@ -48,12 +50,12 @@ namespace Api.Modules
 
                 request.LicensePlateNumberRequest(package,
                     new User("pennyl@lightstone.co.za", "Penny", new Guid("4A17B499-845F-43E2-AA2F-CFCB06920AB6"), null,
-                        null, null), new Context("VVi+ADX+VPi", null, "c99ef6d2-fdea-4a81-b15f-ff8251ac9050"), vehicle,
+                        null, null), new Context(package.Name, null, "c99ef6d2-fdea-4a81-b15f-ff8251ac9050"), vehicle,
                     new Aggregation(Guid.NewGuid()));
 
                 var responses = entryPoint.GetResponsesFromLace(request);
 
-                var packageIdentifier = new PackageIdentifier(packageResponse.Id, new VersionIdentifier(1));
+                var packageIdentifier = new PackageIdentifier(package.Id, new VersionIdentifier(1));
                 var requestIdentifier = new RequestIdentifier(Guid.NewGuid(), SystemIdentifier.CreateApi());
                 var userIdentifier = new UserIdentifier(((IEntity)Context.CurrentUser).Id);
                 var transactionContext = new TransactionContext(Guid.NewGuid(), userIdentifier, requestIdentifier);
--- app/Shared/BuildingBlocks/Configuration/ApiSettings.cs
+++ app/Shared/BuildingBlocks/Configuration/ApiSettings.cs
@@ -17,4 +17,70 @@
             get { return _reader.GetString("api/config/baseUrl", () => "UNKNOWN_BASE_URL"); }
         }
     }
+
+    public class PackageBuilderApiSettings
+    {
+        private readonly AppSettingsReader _reader;
+
+        internal PackageBuilderApiSettings(AppSettingsReader reader)
+        {
+            _reader = reader;
+        }
+
+        /// <summary>
+        /// Finds an appSetting called umApi/config/baseUrl
+        /// </summary>
+        public string BaseUrl
+        {
+            get { return _reader.GetString("pbApi/config/baseUrl", () => "UNKNOWN_BASE_URL"); }
+        }
+    }
+
+    public class UserManagementApiSettings
+    {
+        private readonly AppSettingsReader _reader;
+
+        internal UserManagementApiSettings(AppSettingsReader reader)
+        {
+            _reader = reader;
+        }
+
+        /// <summary>
+        /// Finds an appSetting called umApi/config/baseUrl
+        /// </summary>
+        public string BaseUrl
+        {
+            get { return _reader.GetString("userManagementApi/config/baseUrl", () => "UNKNOWN_BASE_URL"); }
+        }
+    }
+
+    public class UserAuthenticationApiSettings
+    {
+        private readonly AppSettingsReader _reader;
+
+        private UserAuthenticationApiSettings()
+        {
+        }
+
+        internal UserAuthenticationApiSettings(AppSettingsReader reader)
+        {
+            _reader = reader;
+        }
+
+        /// <summary>
+        /// Finds an appSetting called umApi/config/baseUrl
+        /// </summary>
+        public string BaseUrl
+        {
+            get { return _reader.GetString("umApi/config/baseUrl", () => "UNKNOWN_BASE_URL"); }
+        }
+
+        /// <summary>
+        /// Finds an appSetting called umApi/config/redisServers
+        /// </summary>
+        public string RedisServers
+        {
+            get { return _reader.GetString("umApi/config/redisServers", () => "UNKNOWN_REDIS_SERVERS"); }
+        }
+    }
 }
