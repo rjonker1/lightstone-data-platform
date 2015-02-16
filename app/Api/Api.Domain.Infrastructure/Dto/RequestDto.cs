60e43192782289c1f869e1c8d9b282e0ce34eb37
Rudi Jonker <rudi@customapp.co.za>
2015-02-16 09:17:44 +0200

    API - request dto

---
+++ app/Api/Api.Domain.Infrastructure/Dto/RequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Api.Domain.Infrastructure.Dto
+{
+    public class ApiRequestDto
+    {
+        public Guid ContractId { get; private set; }
+        public Guid SourceId { get; private set; }
+
+        public ApiRequestDto()
+        {
+            
+        }
+
+        public ApiRequestDto(Guid contractId, Guid sourceId)
+        {
+            ContractId = contractId;
+            SourceId = sourceId;
+        }
+    }
+}
---
+++ app/Api/Api/Modules/ApiModule.cs
@@ -0,0 +1,14 @@
+﻿using Api.Domain.Infrastructure.Metadata;
+using Nancy;
+
+namespace Api.Modules
+{
+    public class ApiModule : NancyModule
+    {
+        public ApiModule(IApiMetaDataGenerator apiMetaDataGenerator)
+            : base("/api")
+        {
+            Get["/"] = parameters => Response.AsJson(apiMetaDataGenerator.GetAllModuleRouteMetaData(Context));
+        }
+    }
+}
