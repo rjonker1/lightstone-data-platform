edc6a0f6f4ce0e05b303ba7a659aa2e063dc6366
Rudi Jonker <rudi@customapp.co.za>
2015-02-16 11:53:34 +0200

    Added missing file

---
+++ app/UserManagement/UserManagement.Domain.Dtos/ContractPackageDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Domain.Dtos
+{
+    public class ContractPackageResponseDto
+    {
+        public Guid ContractId { get; private set; }
+        public Package Package { get; private set; }
+
+        public ContractPackageResponseDto(Guid contractDurationId, Package package)
+        {
+            ContractId = contractDurationId;
+            Package = package;
+        }
+
+        public ContractPackageResponseDto()
+        {
+
+        }
+    }
+
+    public class ContractPackageRequestDto
+    {
+        public Guid ContractId { get; private set; }
+
+        public ContractPackageRequestDto(Guid contractDurationId)
+        {
+            ContractId = contractDurationId;
+        }
+
+        public ContractPackageRequestDto()
+        {
+
+        }
+    }
+}
