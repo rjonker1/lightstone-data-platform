22722c62539d8334dfa6259d27e50babf515062c
Wayne Munro <waynem@lightstone.co.za>
2015-02-17 16:16:31 +0200

    refactored unit test

--- app/UserManagement/UserManagement.Domain.Entities.Tests/when_persisting_audit_log.cs
+++ app/UserManagement/UserManagement.Domain.Entities.Tests/when_persisting_audit_log.cs
@@ -18,7 +18,7 @@ namespace UserManagement.Domain.Entities.Tests
         }
 
         [Observation]
-        public void should_persist_Audit()
+        public void should_persist_Audit_Role()
         {
             var auditRepo = new Repository<AuditLog>(Session);
            
@@ -85,6 +85,86 @@ namespace UserManagement.Domain.Entities.Tests
             exists = auditRepo.Where(a => a.RecordId == roleId && a.CommitVersion.Value == 3 && a.EventType == "D" && a.EntityName == "Role");
 
             Assert.True(exists.Any());
+
+
+            // userType
+
+            Session = null;
+
+            Session = Container.Resolve<ISessionFactory>().OpenSession();
+
+            var userTypeId = Guid.Parse("618115AF-799D-44DC-96BB-83073D3A1C28");
+
+            // create the role
+
+            Session.BeginTransaction();
+
+            var userTypeRepo = new Repository<UserType>(Session);
+
+            var userType = new UserType("TestUserType " + userTypeId) { Id = userTypeId };
+
+            userTypeRepo.SaveOrUpdate(userType);
+
+            Session.Transaction.Commit();
+
+            Session.Evict(userType);
+
+            // assert exists in the audit log
+
+            exists = auditRepo.Where(a => a.RecordId == userTypeId && a.CommitVersion.Value == 1 && a.EventType == "A" && a.EntityName == "UserType");
+
+            Assert.True(exists.Any());
+
+            // Update
+
+            Session = null;
+            Session = Container.Resolve<ISessionFactory>().OpenSession();
+
+            // fine the role and modify the role
+
+            Session.BeginTransaction();
+
+            userTypeRepo = new Repository<UserType>(Session);
+            userType = userTypeRepo.Get(userTypeId);
+
+            userType.UpdateValue("TestUserTypeX");
+            Session.Transaction.Commit();
+
+            Session.Evict(userType);
+
+            auditRepo = new Repository<AuditLog>(Session);
+
+            exists = auditRepo.Where(a => a.RecordId == userTypeId && a.CommitVersion.Value == 2 && a.EventType == "M" && a.EntityName == "UserType");
+
+            Assert.True(exists.Any());
+
+            // delete
+
+            Session = null;
+
+            Session = Container.Resolve<ISessionFactory>().OpenSession();
+
+            userTypeRepo = new Repository<UserType>(Session);
+            userType = userTypeRepo.Get(userTypeId);
+
+            Session.BeginTransaction();
+            userTypeRepo.Delete(userType);
+            Session.Transaction.Commit();
+
+            auditRepo = new Repository<AuditLog>(Session);
+
+            exists = auditRepo.Where(a => a.RecordId == userTypeId && a.CommitVersion.Value == 3 && a.EventType == "D" && a.EntityName == "UserType");
+
+            Assert.True(exists.Any());
+        }
+
+
+        [Observation]
+        public void should_persist_Audit_UserType()
+        {
+            var auditRepo = new Repository<AuditLog>(Session);
+
+            
         }
 
     }
