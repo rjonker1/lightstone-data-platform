3f85ab2b208175efc634c719362c9c403030ab59
Allistaire Clark <allistairec@lightstone.co.za>
2015-02-25 09:45:32 +0200

    User Management - Soft Delete command | handler setup

---
+++ app/UserManagement/UserManagement.Domain/CommandHandlers/Entities/SoftDeleteEntityHandler.cs
@@ -0,0 +1,18 @@
+﻿using UserManagement.Domain.Core.Entities;
+using UserManagement.Domain.Core.MessageHandling;
+using UserManagement.Domain.Core.Repositories;
+using UserManagement.Domain.Entities.Commands.Entities;
+
+namespace UserManagement.Domain.CommandHandlers.Entities
+{
+    public class SoftDeleteEntityHandler : AbstractMessageHandler<SoftDeleteEntity>
+    {
+
+        private readonly IRepository<Entity> _repository;
+
+        public override void Handle(SoftDeleteEntity command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
