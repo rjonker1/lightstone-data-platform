795023d27f66e2536733d3e495bb25bb2eee57c9
bakerbrian7 <brian@customapp.co.za>
2015-02-25 13:41:32 +0200

    LSAMVP-228 TASK: Implement mapping and saving of Client Contact info, Physical & Postal address
    http://customapp.myjetbrains.com/youtrack/issue/LSAMVP-228

--- app/UserManagement/UserManagement.Domain.Dtos/ClientDto.cs
+++ app/UserManagement/UserManagement.Domain.Dtos/ClientDto.cs
@@ -1,20 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace UserManagement.Domain.Dtos
 {
     public class ClientDto
     {
-        public ClientDto()
-        {
-            Contracts = Enumerable.Empty<NamedEntityDto>();
-        }
-
         public Guid Id { get; set; } 
         public string Name { get; set; }
-        public ContactDetailDto ContactDetailDto { get; set; }
-        public IEnumerable<Guid> ContractIds { get; set; }
-        public IEnumerable<NamedEntityDto> Contracts { get; set; }
+        public Guid ContactDetailId { get; set; }
+        public string ContactDetailLegalEntityName { get; set; }
+        public string ContactDetailAccountsContactName { get; set; }
+        public string ContactDetailEmailAddress { get; set; }
+        public string ContactDetailTelephoneNumber { get; set; }
+        public Guid ContactDetailPhysicalAddressId { get; set; }
+        public string ContactDetailPhysicalAddressType { get; set; }
+        public string ContactDetailPhysicalAddressLine1 { get; set; }
+        public string ContactDetailPhysicalAddressLine2 { get; set; }
+        public string ContactDetailPhysicalAddressSuburb { get; set; }
+        public string ContactDetailPhysicalAddressCity { get; set; }
+        public string ContactDetailPhysicalAddressCountry { get; set; }
+        public string ContactDetailPhysicalAddressPostalCode { get; set; }
+        public Guid ContactDetailPhysicalAddressProvinceId { get; set; }
+        public Guid ContactDetailPostalAddressId { get; set; }
+        public string ContactDetailPostalAddressType { get; set; }
+        public string ContactDetailPostalAddressLine1 { get; set; }
+        public string ContactDetailPostalAddressLine2 { get; set; }
+        public string ContactDetailPostalAddressSuburb { get; set; }
+        public string ContactDetailPostalAddressCity { get; set; }
+        public string ContactDetailPostalAddressCountry { get; set; }
+        public string ContactDetailPostalAddressPostalCode { get; set; }
+        public Guid ContactDetailPostalAddressProvinceId { get; set; }
+        // Work around for AutoMapper to map address as Nancy does not support nested Model binding when using url encoded form posts
+        public AddressDto PhysicalAddressDto { get; set; }
+        // Work around for AutoMapper to map address as Nancy does not support nested Model binding when using url encoded form posts
+        public AddressDto PostalAddressDto { get; set; }
     }
 }
