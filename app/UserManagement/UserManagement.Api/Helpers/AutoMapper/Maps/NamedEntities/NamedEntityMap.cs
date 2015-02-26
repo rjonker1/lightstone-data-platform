419e3dfab6fa221cf8fa4ead34349f0f59cfe93f
bakerbrian7 <brian@customapp.co.za>
2015-02-26 16:42:06 +0200

    LSAMVP-264 TASK: Implement generic saving & updating of all lookup entities
    http://customapp.myjetbrains.com/youtrack/issue/LSAMVP-264

--- app/UserManagement/UserManagement.Api/Helpers/AutoMapper/Maps/NamedEntities/NamedEntityMap.cs
+++ app/UserManagement/UserManagement.Api/Helpers/AutoMapper/Maps/NamedEntities/NamedEntityMap.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using UserManagement.Domain.Core.Entities;
 using UserManagement.Domain.Dtos;
+using UserManagement.Domain.Entities;
 
 namespace UserManagement.Api.Helpers.AutoMapper.Maps.NamedEntities
 {
@@ -13,6 +15,30 @@ namespace UserManagement.Api.Helpers.AutoMapper.Maps.NamedEntities
             Mapper.CreateMap<NamedEntity, NamedEntityDto>();
             Mapper.CreateMap<IEnumerable<NamedEntity>, IEnumerable<NamedEntityDto>>()
                 .ConvertUsing(s => s.Select(Mapper.Map<NamedEntity, NamedEntityDto>));
+
+            Mapper.CreateMap<NamedEntityDto, NamedEntity>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(x => x.Id == new Guid() ? Guid.NewGuid() : x.Id))
+                .Include<NamedEntityDto, PaymentType>()
+                .Include<NamedEntityDto, PlatformStatus>()
+                .Include<NamedEntityDto, CreateSource>()
+                .Include<NamedEntityDto, CommercialState>()
+                .Include<NamedEntityDto, ContractType>()
+                .Include<NamedEntityDto, EscalationType>()
+                .Include<NamedEntityDto, ContractDuration>()
+                .Include<NamedEntityDto, Province>()
+                .Include<NamedEntityDto, UserType>()
+                .Include<NamedEntityDto, Role>();
+
+            Mapper.CreateMap<NamedEntityDto, PaymentType>();
+            Mapper.CreateMap<NamedEntityDto, PlatformStatus>();
+            Mapper.CreateMap<NamedEntityDto, CreateSource>();
+            Mapper.CreateMap<NamedEntityDto, CommercialState>();
+            Mapper.CreateMap<NamedEntityDto, ContractType>();
+            Mapper.CreateMap<NamedEntityDto, EscalationType>();
+            Mapper.CreateMap<NamedEntityDto, ContractDuration>();
+            Mapper.CreateMap<NamedEntityDto, Province>();
+            Mapper.CreateMap<NamedEntityDto, UserType>();
+            Mapper.CreateMap<NamedEntityDto, Role>();
         }
     }
 }
--- app/UserManagement/UserManagement.Api/Modules/LookupModule.cs
+++ app/UserManagement/UserManagement.Api/Modules/LookupModule.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
+using MemBus;
 using Nancy;
 using Nancy.ModelBinding;
 using Nancy.Responses.Negotiation;
 using UserManagement.Api.ViewModels;
 using UserManagement.Domain.Core.Entities;
 using UserManagement.Domain.Core.Helpers;
+using UserManagement.Domain.Core.Repositories;
 using UserManagement.Domain.Dtos;
+using UserManagement.Domain.Entities.Commands.Entities;
 using UserManagement.Infrastructure.Helpers;
 
 namespace UserManagement.Api.Modules
 {
     public class LookupModule : NancyModule
     {
-        public LookupModule(IRetrieveEntitiesByType entityRetriever)
+        public LookupModule(IBus bus, IRetrieveEntitiesByType entityRetriever, INamedEntityRepository<NamedEntity> entities)
         {
             Get["/Lookups/{type}"] = parameters =>
             {
@@ -29,6 +33,40 @@ namespace UserManagement.Api.Modules
                     .WithView("Index")
                     .WithModel(new LookupViewModel(type))
                     .WithMediaRangeModel(MediaRange.FromString("application/json"), dto);
+            };
+
+            Get["/Lookups/Add/{type}"] = parameters =>
+            {
+                return View["Save", new NamedEntityDto{AssemblyQualifiedName = parameters.type.ToString()}];
+            };
+
+            Post["/Lookups"] = _ =>
+            {
+                var dto = this.Bind<NamedEntityDto>();
+                var entity = (NamedEntity)Mapper.Map(dto, entities.Get(dto.Id), typeof(NamedEntityDto), Type.GetType(dto.AssemblyQualifiedName));
+
+                bus.Publish(new CreateUpdateEntity(entity, true));
+
+                return Response.AsJson(dto.AssemblyQualifiedName);
+            };
+
+            Get["/Lookups/{id:guid}"] = parameters =>
+            {
+                var id = (Guid)parameters.id;
+                var dto = Mapper.Map<NamedEntity, NamedEntityDto>(entities.First(x => x.Id == id)); //todo: repo.Get() not loading entity
+
+                return View["Save", dto];
+            };
+
+            Post["/Lookups/{id}"] = _ =>
+            {
+                var dto = this.Bind<NamedEntityDto>();
+                var namedEntity = entities.First(x => x.Id == dto.Id);
+                var entity = (NamedEntity)Mapper.Map(dto, namedEntity, typeof(NamedEntityDto), namedEntity.GetType());
+
+                bus.Publish(new CreateUpdateEntity(entity, false));
+
+                return Response.AsJson(namedEntity.GetType().AssemblyQualifiedName);
             };
 
             Get["/Lookups/{type}/{filter}"] = parameters =>
