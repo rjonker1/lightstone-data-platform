6699c80b8562c88557101f2a03ef0c5c20e5a46c
Allistaire Clark <allistairec@lightstone.co.za>
2015-02-27 14:05:21 +0200

    Refactored SoftDeleteClientRuleHandler

--- app/UserManagement/UserManagement.Domain/BusinessRules/Clients/SoftDeleteClientRuleHandler.cs
+++ app/UserManagement/UserManagement.Domain/BusinessRules/Clients/SoftDeleteClientRuleHandler.cs
@@ -22,7 +22,8 @@ namespace UserManagement.Domain.BusinessRules.Clients
         {
 
             var entity = command.Entity;
-            var hasClientUser = _clientUsers.Where(x => x.Client.Id.Equals(entity.Id));
+            var hasClientUser = _clientUsers.Where(x => x.Client.Id.Equals(entity.Id)).Select(u => u.User).Where(usr => usr.IsActive != null && usr.IsActive.Value.Equals(true)).ToList();
+            //var hasClientUser = _clientUsers.Where(x => x.Client.Id.Equals(entity.Id));
 
             if (hasClientUser.Any())
             {
