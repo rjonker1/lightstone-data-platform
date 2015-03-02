31ad8ff29ee37dd156259aa5771077a025b0c5fc
Allistaire Clark <allistairec@lightstone.co.za>
2015-03-02 14:17:58 +0200

    Contract BR contraints

--- app/UserManagement/UserManagement.Domain/BusinessRules/Contracts/SoftDeleteContractRuleHandler.cs
+++ app/UserManagement/UserManagement.Domain/BusinessRules/Contracts/SoftDeleteContractRuleHandler.cs
@@ -25,6 +25,20 @@ namespace UserManagement.Domain.BusinessRules.Contracts
 
             var hasClients = _contracts.Where(x => x.Id.Equals(entity.Id)).Select(u => u.Clients.Where(cl => cl.IsActive.Equals(true))).ToList();
             var hasCustomers = _contracts.Where(x => x.Id.Equals(entity.Id)).Select(u => u.Customers.Where(cus => cus.IsActive.Equals(true))).ToList();
+
+            if (hasClients.Any())
+            {
+                var exception = new LightstoneAutoException("Contract cannot be deleted due to Client constraint".FormatWith(entity.GetType().Name));
+                this.Warn(() => exception);
+                throw exception;
+            }
+
+            if (hasCustomers.Any())
+            {
+                var exception = new LightstoneAutoException("Contract cannot be deleted due to Customer constraint".FormatWith(entity.GetType().Name));
+                this.Warn(() => exception);
+                throw exception;
+            }
         }
     }
 }
