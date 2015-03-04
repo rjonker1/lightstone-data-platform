8cc62431854719d048c8996b14fd0a3290a5b2be
bakerbrian7 <brian@customapp.co.za>
2015-03-04 07:33:57 +0200

    Refactoring

--- app/UserManagement/UserManagement.Api/Helpers/AutoMapper/Maps/Users/UserMap.cs
+++ app/UserManagement/UserManagement.Api/Helpers/AutoMapper/Maps/Users/UserMap.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
+using Microsoft.Practices.ServiceLocation;
 using UserManagement.Domain.Core.Entities;
+using UserManagement.Domain.Core.Repositories;
 using UserManagement.Domain.Dtos;
 using UserManagement.Domain.Entities;
 
@@ -22,9 +24,15 @@ namespace UserManagement.Api.Helpers.AutoMapper.Maps.Users
 
             Mapper.CreateMap<UserDto, User>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(x => x.Id == new Guid() ? Guid.NewGuid() : x.Id))
-                .ForMember(dest => dest.UserType, opt => opt.MapFrom(x => Mapper.Map<Tuple<Guid, Type>, Entity>(new Tuple<Guid, Type>(x.UserTypeId, typeof (UserType)))))
-                .ForMember(dest => dest.Roles, opt => opt.MapFrom(x => x.RoleIds != null ? new HashSet<Role>(x.RoleIds.Select(id => (Role)Mapper.Map<Tuple<Guid, Type>, Entity>(new Tuple<Guid, Type>(id, typeof(Role))))) : Enumerable.Empty<Role>()))
-                .ForMember(dest => dest.Customers, opt => opt.MapFrom(x => x.CustomerIds != null ? new HashSet<Customer>(x.CustomerIds.Select(id => (Customer)Mapper.Map<Tuple<Guid, Type>, Entity>(new Tuple<Guid, Type>(id, typeof(Customer))))) : Enumerable.Empty<Customer>()))
+                .ForMember(dest => dest.UserType, opt => opt.MapFrom(x => ServiceLocator.Current.GetInstance<IValueEntityRepository<UserType>>().Get(x.UserTypeId)))
+                .ForMember(dest => dest.Roles, opt => opt.MapFrom(x => 
+                    x.RoleIds != null
+                        ? new HashSet<Role>(x.RoleIds.Select(id => ServiceLocator.Current.GetInstance<IValueEntityRepository<Role>>().Get(id))) 
+                        : Enumerable.Empty<Role>()))
+                .ForMember(dest => dest.Customers, opt => opt.MapFrom(x => 
+                    x.CustomerIds != null
+                        ? new HashSet<Customer>(x.CustomerIds.Select(id => ServiceLocator.Current.GetInstance<INamedEntityRepository<Customer>>().Get(id))) 
+                        : Enumerable.Empty<Customer>()))
                 .ForMember(dest => dest.ClientUsers, opt => opt.MapFrom(x => Mapper.Map<IEnumerable<ClientUserDto>, IEnumerable<ClientUser>>(x.ClientUsers)))
                 .AfterMap((src, dest) =>
                 {
