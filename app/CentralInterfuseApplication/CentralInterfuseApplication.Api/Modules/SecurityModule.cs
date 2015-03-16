e149623247392591f5b5d6ab40af0f18fb62a44e
bakerbrian7 <brian@customapp.co.za>
2015-03-16 14:57:04 +0200

    Initial check in of CIA suite

---
+++ app/CentralInterfuseApplication/CentralInterfuseApplication.Api/Modules/SecurityModule.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using Shared.BuildingBlocks.Api.ApiClients;
+
+namespace CentralInterfuseApplication.Api.Modules
+{
+    public class SecurityModule : NancyModule
+    {
+        public SecurityModule(IUserManagementApiClient client)
+        {
+            Get["/login"] = parameters =>
+            {
+                return View["Index"];
+            };
+
+            Get["/logout"] = parameters =>
+            {
+                return null;
+            };
+
+            Post["/login"] = parameters =>
+            {
+                //client.Post<ApiUser>("", "/login", )
+
+
+                return Response.AsRedirect("/");
+            };
+        }
+    }
+}
