72a4bc66b5c922a532e3da45fcf83e314e95e37c
bakerbrian7 <brian@customapp.co.za>
2015-03-27 11:21:44 +0200

    Fixed AutoMapper Missing mapping exception

--- app/PackageBuilder/PackageBuilder.Api/Helpers/AutoMapper/Maps/DataFields/ToDataFieldMap.cs
+++ app/PackageBuilder/PackageBuilder.Api/Helpers/AutoMapper/Maps/DataFields/ToDataFieldMap.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using PackageBuilder.Domain.Dtos.Write;
+using PackageBuilder.Domain.Entities.Contracts.DataFields.Write;
 using PackageBuilder.Domain.Entities.DataFields.Write;
 
 namespace PackageBuilder.Api.Helpers.AutoMapper.Maps.DataFields
@@ -11,8 +12,11 @@ namespace PackageBuilder.Api.Helpers.AutoMapper.Maps.DataFields
     {
         public void CreateMaps()
         {
-            Mapper.CreateMap<IEnumerable<DataProviderFieldItemDto>, IEnumerable<DataField>>()
-                .ConvertUsing(s => s == null ? Enumerable.Empty<DataField>() : s.Select(Mapper.Map<DataProviderFieldItemDto, DataField>).ToList());
+            //Mapper.CreateMap<IEnumerable<DataProviderFieldItemDto>, IEnumerable<DataField>>()
+            //    .ConvertUsing(s => s == null ? Enumerable.Empty<DataField>() : s.Select(Mapper.Map<DataProviderFieldItemDto, DataField>).ToList());
+
+            Mapper.CreateMap<DataProviderFieldItemDto, IDataField>()
+                .ConstructUsing(Mapper.Map<DataProviderFieldItemDto, DataField>);
             Mapper.CreateMap<DataProviderFieldItemDto, DataField>()
                     .ForMember(d => d.Type, opt => opt.ResolveUsing(x => Mapper.Map<string, Type>(x.Type))) 
                     .ForMember(d => d.DataFields, opt => opt.MapFrom(x => x.DataFields == null ? Enumerable.Empty<DataField>() : Mapper.Map<IEnumerable<DataProviderFieldItemDto>, IEnumerable<DataField>>(x.DataFields.ToList())));
--- app/PackageBuilder/PackageBuilder.Api/Helpers/AutoMapper/Maps/LaceResponseMap.cs
+++ app/PackageBuilder/PackageBuilder.Api/Helpers/AutoMapper/Maps/LaceResponseMap.cs
@@ -6,11 +6,9 @@ using Lace.Domain.Core.Contracts.Requests;
 using PackageBuilder.Domain.Entities;
 using PackageBuilder.Domain.Entities.Contracts.DataFields.Write;
 using PackageBuilder.Domain.Entities.Contracts.DataProviders.Write;
-using PackageBuilder.Domain.Entities.Contracts.Packages.Write;
 using PackageBuilder.Domain.Entities.DataFields.Write;
 using PackageBuilder.Domain.Entities.DataProviders.Write;
 using PackageBuilder.Domain.Entities.Enums.DataProviders;
-using PackageBuilder.Domain.Entities.Packages.Write;
 
 namespace PackageBuilder.Api.Helpers.AutoMapper.Maps
 {
@@ -18,9 +16,6 @@ namespace PackageBuilder.Api.Helpers.AutoMapper.Maps
     {
         public void CreateMaps()
         {
-            Mapper.CreateMap<IPackage, Package>()
-                .ForMember(d => d.DataProviders, opt => opt.MapFrom(x => x.DataProviders.Select(Mapper.Map<IDataProvider, DataProvider>).ToList()));
-
             Mapper.CreateMap<IPointToLaceProvider, DataProviderName>()
                 .Include<IProvideDataFromIvid, DataProviderName>()
                 .Include<IProvideDataFromIvidTitleHolder, DataProviderName>()
@@ -57,42 +52,9 @@ namespace PackageBuilder.Api.Helpers.AutoMapper.Maps
                     }
                 })
             .ForMember(d => d.DataFields, opt => opt.Ignore());
-            //.ForMember(d => d.DataFields, opt => opt.ResolveUsing(x => Mapper.Map<IEnumerable<IDataField>, IEnumerable<DataField>>(x.DataFields)));
-            //.ForMember(d => d.DataFields, opt => opt.MapFrom(x => Mapper.Map(x.DataFields, typeof(IEnumerable<DataField>))));
-            //.ForMember(d => d.DataFields, opt => opt.MapFrom(x => x.DataFields.Select(f => Mapper.Map(f, typeof(DataField)))));
-            //.ForMember(d => d.DataFields, opt => opt.MapFrom(x => x.DataFields.SelectHierarchy(fld => fld.DataFields, fld => fld.IsSelected == true)))
-            //.ForMember(d => d.DataFields, opt => opt.MapFrom(x => Mapper.Map<IEnumerable<IDataField>, IEnumerable<DataField>>(x.DataFields.SelectHierarchy(fld => fld.DataFields, fld => fld.IsSelected == true))))
-            //.ForMember(d => d.DataFields, opt => opt.MapFrom(x => x.DataFields.SelectHierarchy(fld => fld.DataFields, fld => fld.IsSelected == true).Select(Mapper.Map<IDataField, DataField>)))
-            //.AfterMap((s, d) =>
-            //{
-            //    d.DataFields = d.DataFields.SelectHierarchy(fld => fld.DataFields, fld => fld.IsSelected == true).ToList();
-            //    if (!d.DataFields.Any())
-            //        d = null;
-            //});
 
-            //Mapper.CreateMap<IEnumerable<IDataField>, IEnumerable<DataField>>()
-            //    .BeforeMap((s, d) =>
-            //    {
-            //        d = s.Traverse()
-            //            .Select(x => Mapper.Map(x, d.Traverse()
-            //                .SelectHierarchy(f => f.DataFields, f => f.Namespace == x.Namespace).FirstOrDefault())).Cast<DataField>();
-            //    })
-             //.ConvertUsing<ITypeConverter<IEnumerable<IDataField>, IEnumerable<DataField>>>();
-               //.ConvertUsing(s => s != null ? s.Select(Mapper.Map<IDataField, DataField>) : Enumerable.Empty<DataField>());
-               //.ConvertUsing(s => s != null ? s.Select(x => Mapper.Map(x, typeof(DataField))).Cast<DataField>() : Enumerable.Empty<DataField>());
             Mapper.CreateMap<IDataField, DataField>()
-                .BeforeMap((s, d) =>
-                {
-
-                })
                 .ForMember(d => d.IsSelected, opt => opt.Ignore());
-            //.ForMember(d => d.IsSelected, opt => opt.MapFrom(x => Mapper.Map<IDataField, bool>(x)))
-            //.ForMember(d => d.DataFields, opt => opt.MapFrom(x => x.DataFields.Where(fld => fld.IsSelected == true)))
-            //.ForMember(d => d.DataFields, opt => opt.MapFrom(x => Mapper.Map(x.DataFields, typeof(IEnumerable<IDataField>))));
-            //.ForMember(d => d.DataFields, opt => opt.MapFrom(x => Mapper.Map<IEnumerable<IDataField>, IEnumerable<DataField>>(x.DataFields)));
-
-            //Mapper.CreateMap<IDataField, bool>()
-            //    .ConvertUsing<ITypeConverter<IDataField, bool>>();
         }
     }
 }
