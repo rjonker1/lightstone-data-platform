0d8261e0873057027e5ff5fa077e70bf7624aee5
Rudi Jonker <rudi@customapp.co.za>
2015-04-01 10:51:56 +0200

    Lace Request - add different requests for different search types

---
+++ app/Lace/Source/Lace.Domain.Core.Requests/Contracts/IAmLicensePlateRequest.cs
@@ -0,0 +1,13 @@
+﻿using Lace.Domain.Core.Contracts.Requests;
+using PackageBuilder.Domain.Entities.Contracts.Packages.Write;
+
+namespace Lace.Domain.Core.Requests.Contracts
+{
+    public interface IAmLicensePlateRequest : IPointToLaceRequest
+    {
+        IProvideUserInformationForRequest User { get; }
+        IProvideVehicleInformationForRequest Vehicle { get; }
+        IProvideRequestAggregation Aggregation { get; }
+        IPackage Package { get; }
+    }
+}
