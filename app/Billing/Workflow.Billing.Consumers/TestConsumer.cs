b303c40b715f1c0756979e4201fc344ad8a10172
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-02 15:12:13 +0200

    Base consumer RabbitMQ

---
+++ app/Billing/Workflow.Billing.Consumers/TestConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using EasyNetQ.AutoSubscribe;
+using Workflow.Billing.Domain;
+using Workflow.Billing.Messages;
+using Workflow.Billing.Repository;
+
+namespace Workflow.Billing.Consumers
+{
+    public class TestConsumer : IConsume<TestMessage>
+    {
+
+        public void Consume(TestMessage message)
+        {
+        }
+    }
+}
---
+++ app/Billing/Workflow.Billing.Messages/TestMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+using DataPlatform.Shared.Messaging;
+
+namespace Workflow.Billing.Messages
+{
+    [Serializable]
+    [DataContract]
+    public class TestMessage : IPublishableMessage
+    {
+        public TestMessage()
+        {
+        }
+    }
+}
