d9bb9b19af07ea9f21af46ae14524065dde4960a
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-02 11:01:19 +0200

    Added Billing.Scheduler

---
+++ app/Billing/Billing.Scheduler/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
+using Owin;
+
+namespace Billing.Scheduler
+{
+    public class Bootstrapper
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfire(config =>
+            {
+                config.UseSqlServerStorage("Server=.; Database=Hangfire.Test; Integrated Security=SSPI;");
+                config.UseServer();
+            });
+
+            RecurringJob.AddOrUpdate(() => Console.Write("Integrity Testeroonie"), "*/5 * * * *");
+        }
+    }
+}
