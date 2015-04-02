42dc1861751c720ebaaa5f54aa9735cc3e420824
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-02 07:41:33 +0200

    Added PackageBuilderApi integration

---
+++ app/Billing/Billing.Api/Modules/PackageModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Nancy;
+using Newtonsoft.Json;
+using Shared.BuildingBlocks.Api.ApiClients;
+
+namespace Billing.Api.Modules
+{
+    public class PackageModule : NancyModule
+    {
+        public PackageModule(IPackageBuilderApiClient packageBuilderApiClient)
+        {
+            Get["/Packages/{filter}"] = parameters =>
+            {
+                var packagesJson = packageBuilderApiClient.Get("", "Packages", new { parameters.filter });
+                //var packages = JsonConvert.DeserializeObject<IEnumerable<PackageDto>>(packagesJson);
+
+                return packagesJson;
+            };
+        }
+    }
+}
