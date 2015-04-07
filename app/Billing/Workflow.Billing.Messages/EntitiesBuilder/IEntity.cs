9924ef5361975797d0f0f6ac0764a8faee4beb09
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-07 12:23:47 +0200

    Messages build up

---
+++ app/Billing/Workflow.Billing.Messages/EntitiesBuilder/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using DataPlatform.Shared.Messaging;
+
+namespace Workflow.Billing.Messages.EntitiesBuilder
+{
+    public interface IEntity : IPublishableMessage
+    {
+        Guid Id { get; }
+    }
+}
