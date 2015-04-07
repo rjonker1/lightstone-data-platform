ba75dad26c4fb65a2372d50fcce1af5bb077a80a
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-07 12:52:04 +0200

    Shared lib restructure

---
+++ app/Shared/Shared.Messaging.Billing/Messages/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataPlatform.Shared.Messaging;
+
+namespace Shared.Messaging.Billing.Messages
+{
+    public class Entity : IEntity, IPublishableMessage
+    {
+        public Guid Id { get; set; }
+
+        protected internal Entity() { }
+
+        protected Entity(Guid id)
+        {
+            Id = id == new Guid() ? Guid.NewGuid() : id;
+        }
+
+        public DateTime? Modified { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? Created { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ app/Shared/Shared.Messaging.Billing/Messages/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Shared.Messaging.Billing.Messages
+{
+    public interface IEntity
+    {
+        Guid Id { get; } 
+    }
+}
