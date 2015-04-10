1fbb5371ef106b0b738cf68a2038a3112a977d31
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-10 07:40:17 +0200

    Unit tests

---
+++ app/Billing/Billing.Unit.Tests/Consumers/Entity/when_receiving_new_user_message.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Moq;
+using Shared.Messaging.Billing.Messages;
+using Workflow.Billing.Consumers;
+using Workflow.Billing.Domain.Entities;
+using Workflow.Billing.Repository;
+using Xunit.Extensions;
+
+namespace Billing.Api.Tests.Consumers.Entity
+{
+    public class when_receiving_new_user_message : Specification
+    {
+        private readonly EntityConsumer consumer;
+        private readonly UserMessage message;
+        private readonly Mock<IRepository<UserMeta>> repository;
+        private Exception thrownException;
+
+        public when_receiving_new_user_message()
+        {
+            repository = new Mock<IRepository<UserMeta>>();
+            consumer = new EntityConsumer(repository.Object);
+            message = new UserMessage();
+        }
+
+        public override void Observe()
+        {
+            try
+            {
+                //consumer.Consume(message);
+            }
+            catch (Exception e)
+            {
+                thrownException = e;
+            }
+        }
+
+        [Observation]
+        public void should_consume_message()
+        {
+            //thrownException.ShouldBeNull();
+        }
+    }
+}
