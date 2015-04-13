2eeaee4bd870f98910a8aea1f5f131982c0eb749
Rudi Jonker <rudi@customapp.co.za>
2015-04-13 20:03:28 +0200

    shared billing - transaction created message

---
+++ app/Shared/Shared.Messaging.Billing/Messages/InvoiceTransactionCreated.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+using NServiceBus;
+
+namespace DataPlatform.Shared.Messaging.Billing.Messages
+{
+    [Serializable]
+    [DataContract]
+    public class BillingTransactionCreated : IEvent
+    {
+        public BillingTransactionCreated(Guid transactionId)
+        {
+            TransactionId = transactionId;
+        }
+
+        [DataMember]
+        public Guid TransactionId { get; private set; }
+    }
+}
