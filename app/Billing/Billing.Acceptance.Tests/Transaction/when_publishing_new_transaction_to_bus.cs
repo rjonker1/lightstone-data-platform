e65fc8b0a02033d5b54388ca70d46fd96279b2d2
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-14 14:34:07 +0200

    Nuget package refs

--- app/Billing/Billing.Acceptance.Tests/Transaction/when_publishing_new_transaction_to_bus.cs
+++ app/Billing/Billing.Acceptance.Tests/Transaction/when_publishing_new_transaction_to_bus.cs
@@ -1,38 +1,37 @@
 ﻿using System;
-//using DataPlatform.Shared.Messaging.Billing.Messages;
-//using EasyNetQ;
-//using EasyNetQ.Topology;
-//using Shared.Messaging.Billing.Helpers;
-//using Workflow.BuildingBlocks;
+using DataPlatform.Shared.Messaging.Billing.Messages;
+using EasyNetQ;
+using Shared.Messaging.Billing.Helpers;
+using Workflow.BuildingBlocks;
 using Xunit.Extensions;
 
 namespace Billing.Acceptance.Tests.Transaction
 {
-    //public class when_publishing_new_transaction_to_bus : Specification
-    //{
-    //    private readonly IAdvancedBus _bus;
-    //    private InvoiceTransactionCreated transaction;
+    public class when_publishing_new_transaction_to_bus : Specification
+    {
+        private readonly IAdvancedBus _bus;
+        private InvoiceTransactionCreated transaction;
 
-    //    public when_publishing_new_transaction_to_bus()
-    //    {
-    //        _bus = BusFactory.CreateAdvancedBus("workflow/billing/queue");
-    //    }
+        public when_publishing_new_transaction_to_bus()
+        {
+            _bus = BusFactory.CreateAdvancedBus("workflow/billing/queue");
+        }
 
-    //    public override void Observe()
-    //    {
-    //        var bus = new TransactionBus(_bus);
+        public override void Observe()
+        {
+            var bus = new TransactionBus(_bus);
 
-    //        transaction = new InvoiceTransactionCreated(Guid.NewGuid());
+            transaction = new InvoiceTransactionCreated(Guid.NewGuid());
 
-    //        //var message = (IMessage<InvoiceTransactionCreated>) (transaction);
+            //var message = (IMessage<InvoiceTransactionCreated>) (transaction);
 
-    //        bus.Send(transaction, "TESTEXCHANGE1", "TESTQUEUE1");
-    //    }
+            bus.Send(transaction, "TESTEXCHANGE1", "TESTQUEUE1");
+        }
 
-    //    [Observation]
-    //    public void it_should_publish_a_transaction()
-    //    {
-    //        _bus.ShouldNotBeNull();
-    //    }
-    //}
+        [Observation]
+        public void it_should_publish_a_transaction()
+        {
+            _bus.ShouldNotBeNull();
+        }
+    }
 }
