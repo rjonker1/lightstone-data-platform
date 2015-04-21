872bebad4731f16cd1e82d894b26fa30391bbfd9
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-21 14:56:22 +0200

    Added BillRunConsumer. Implemented base mapping for PreBilling to StageBilling repository

---
+++ app/Billing/Workflow.Billing.Consumers/ConsumerTypes/BillRunConsumer.cs
@@ -0,0 +1,23 @@
+﻿using DataPlatform.Shared.Repositories;
+using EasyNetQ;
+using Workflow.Billing.Domain.Entities;
+
+namespace Workflow.Billing.Consumers.ConsumerTypes
+{
+    public class BillRunConsumer
+    {
+        private readonly IRepository<PreBilling> _preBillingrRepository; 
+        private readonly IRepository<StageBilling> _stageBillingRepository;
+
+        public BillRunConsumer(IRepository<PreBilling> preBillingrRepository, IRepository<StageBilling> stageBillingRepository)
+        {
+            _preBillingrRepository = preBillingrRepository;
+            _stageBillingRepository = stageBillingRepository;
+        }
+
+        public void Consume()
+        {
+            
+        }
+    }
+}
---
+++ app/Billing/Workflow.Billing/Helpers/AutoMapper/Maps/StageBillingMap.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DataPlatform.Shared.Repositories;
+using Workflow.Billing.Domain.Entities;
+
+namespace Workflow.Billing.Helpers.AutoMapper.Maps
+{
+    public class StageBillingMap : ICreateAutoMapperMaps
+    {
+        public void CreateMaps()
+        {
+            Mapper.CreateMap<IRepository<PreBilling>, IRepository<StageBilling>>();
+        }
+    }
+}
