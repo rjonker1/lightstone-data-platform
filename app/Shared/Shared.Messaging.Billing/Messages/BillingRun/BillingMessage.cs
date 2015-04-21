18714c2f387266ae4f827510d2a271701bb3b31d
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-21 14:35:05 +0200

    Added BillingMessage for BillingRun invocation

---
+++ app/Shared/Shared.Messaging.Billing/Messages/BillingRun/BillingMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DataPlatform.Shared.Messaging.Billing.Helpers;
+using EasyNetQ;
+
+namespace DataPlatform.Shared.Messaging.Billing.Messages.BillingRun
+{
+    [Queue("DataPlatform.Transactions.Billing", ExchangeName = "DataPlatform.Transactions.Billing")]
+    public class BillingMessage : Entity
+    {
+        public virtual string RunType { get; set; }
+        public virtual DateTime Schedule { get; set; }
+
+        public BillingMessage()
+        {
+        }
+    }
+}
