f479eaff3757ac259a720416a63260cca79a0b01
bakerbrian7 <brian@customapp.co.za>
2015-04-24 08:16:44 +0200

    LSAMVP-354 TASK: Data provider request formulation

--- app/Lace/Source/Lace.Domain.Core/Contracts/DataProviders/IProvideDataFromLightstoneBusiness.cs
+++ app/Lace/Source/Lace.Domain.Core/Contracts/DataProviders/IProvideDataFromLightstoneBusiness.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
-using Lace.Domain.Core.Contracts.DataProviders.Property;
+using Lace.Domain.Core.Contracts.DataProviders.Business;
 using Lace.Domain.Core.Contracts.Requests;
 
 namespace Lace.Domain.Core.Contracts.DataProviders
 {
-    public interface IProvideDataFromLightstoneProperty : IPointToLaceProvider
+    public interface IProvideDataFromLightstoneBusiness : IPointToLaceProvider
     {
-        IEnumerable<IRespondWithProperty> PropertyInformation { get; }
     }
 }
--- app/Lace/Source/Lace.Domain.Core/Contracts/DataProviders/IProvideDataFromLightstoneProperty.cs
+++ app/Lace/Source/Lace.Domain.Core/Contracts/DataProviders/IProvideDataFromLightstoneProperty.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using Lace.Domain.Core.Contracts.DataProviders.Property;
 using Lace.Domain.Core.Contracts.Requests;
-using Lace.Domain.Core.Requests.Contracts;
 using Lace.Domain.Core.Requests.Contracts.Requests;
 
 namespace Lace.Domain.Core.Contracts.DataProviders
