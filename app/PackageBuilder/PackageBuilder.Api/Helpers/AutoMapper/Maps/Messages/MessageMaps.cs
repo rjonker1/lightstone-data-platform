577c762012df705d0dea219f313f1f1fd554c5df
Allistaire Clark <allistairec@lightstone.co.za>
2015-04-30 14:12:28 +0200

    Added PackageMessage mapping

---
+++ app/PackageBuilder/PackageBuilder.Api/Helpers/AutoMapper/Maps/Messages/MessageMaps.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DataPlatform.Shared.Messaging.Billing.Messages;
+using PackageBuilder.Domain.Dtos.Write;
+
+namespace PackageBuilder.Api.Helpers.AutoMapper.Maps.Messages
+{
+    public class MessageMaps : ICreateAutoMapperMaps
+    {
+        public void CreateMaps()
+        {
+            Mapper.CreateMap<PackageDto, PackageMessage>()
+                .ForMember(dest => dest.PackageId, opt => opt.MapFrom(x => x.Id))
+                .ForMember(dest => dest.PackageName, opt => opt.MapFrom(x => x.Name));
+        }
+    }
+}
