eb2e9b011aed7d7d39133fd71570cc4e219addbf
Allistaire Clark <allistairec@lightstone.co.za>
2015-05-01 16:57:46 +0200

    Reporting

---
+++ app/Reporting/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace Reporting
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
