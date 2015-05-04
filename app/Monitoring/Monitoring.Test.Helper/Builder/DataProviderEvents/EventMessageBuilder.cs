d218b186fa480f71a8d1f98174bc43e0af980e67
Allistaire Clark <allistairec@lightstone.co.za>
2015-05-04 13:15:28 +0200

    Build fix

--- app/Monitoring/Monitoring.Test.Helper/Builder/DataProviderEvents/EventMessageBuilder.cs
+++ app/Monitoring/Monitoring.Test.Helper/Builder/DataProviderEvents/EventMessageBuilder.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using DataPlatform.Shared.Enums;
-using DataPlatform.Shared.Messaging.Events;
+//using DataPlatform.Shared.Messaging.Events;
 
 namespace Monitoring.Test.Helper.Builder.DataProviderEvents
 {
     public class DataProviderEvents
     {
-        public MonitoringEvent ForDataProviderEvent(Guid aggreateId,string payload, DateTime date )
-        {
-            return new MonitoringEvent(aggreateId, payload,date, MonitoringSource.Lace);
-        }
+        //public MonitoringEvent ForDataProviderEvent(Guid aggreateId,string payload, DateTime date )
+        //{
+        //    return new MonitoringEvent(aggreateId, payload,date, MonitoringSource.Lace);
+        //}
 
     }
 }
