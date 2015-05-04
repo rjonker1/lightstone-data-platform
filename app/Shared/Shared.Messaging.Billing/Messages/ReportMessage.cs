a3663c93223d02c339b0da647834547a287bc08e
Allistaire Clark <allistairec@lightstone.co.za>
2015-05-04 12:59:08 +0200

    ReportMessage

---
+++ app/Shared/Shared.Messaging.Billing/Messages/ReportMessage.cs
@@ -0,0 +1,16 @@
+﻿using DataPlatform.Shared.Messaging.Billing.Helpers;
+using EasyNetQ;
+using RestSharp;
+
+namespace DataPlatform.Shared.Messaging.Billing.Messages
+{
+    [Queue("DataPlatform.Reports.Billing", ExchangeName = "DataPlatform.Reports.Billing")]
+    public class ReportMessage : Entity
+    {
+        public virtual JsonObject reportBody {get; set; }
+
+        public ReportMessage()
+        {
+        }
+    }
+}
