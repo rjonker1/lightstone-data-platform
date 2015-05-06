c2049d51c8763358a4890b39ce1626fd0c1e1fff
Allistaire Clark <allistairec@lightstone.co.za>
2015-05-06 15:16:53 +0200

    BillRunConsumer FinalBilling refactoring

--- app/Billing/Workflow.Billing/Domain/Entities/FinalBilling.cs
+++ app/Billing/Workflow.Billing/Domain/Entities/FinalBilling.cs
@@ -5,7 +5,7 @@ namespace Workflow.Billing.Domain.Entities
     public class FinalBilling: User
     {
         public virtual int BillingId { get; set; }
-        public virtual Guid PreBillingId { get; set; }
+        public virtual Guid StageBillingId { get; set; }
 
         public FinalBilling()
         {
