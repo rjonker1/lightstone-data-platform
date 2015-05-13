813422ab736209eb2b4bd1ceee8a8e311a2e33ea
Allistaire Clark <allistaire.clark@yahoo.com>
2015-05-13 18:43:59 +0200

    InvoiceCsvData class added to Reporting

--- app/Reporting/Workflow.Reporting/Entities/InvoiceCsvData.cs
+++ app/Reporting/Workflow.Reporting/Entities/InvoiceCsvData.cs
@@ -2,6 +2,19 @@
 {
     public class InvoiceCsvData
     {
-         
+         public string DOCTYPE { get; set; }
+         public string INVNUMBER { get; set; }
+         public string ACCOUNTID { get; set; }
+         public string DESCRIPTION { get; set; }
+         public string INVDATE { get; set; }
+         public string TAXINCLUSIVE { get; set; }
+         public string ORDERNUM { get; set; }
+         public string CDESCRIPTION { get; set; }
+         public string FQUANTITY { get; set; }
+         public string FUNITPRICEEXCL { get; set; }
+         public string ITAXTYPEID { get; set; }
+         public string ISTOCKCODEID { get; set; }
+         public string Project { get; set; }
+         public string Tax_Number { get; set; }
     }
 }
