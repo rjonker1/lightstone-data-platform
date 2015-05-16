62b02a7a5a36c5be2955ee8acb5ead72c233d25f
Allistaire Clark <allistaire.clark@yahoo.com>
2015-05-16 19:16:20 +0200

    Meta

--- app/Billing/Billing.Api/Modules/AdminBillingModule.cs
+++ app/Billing/Billing.Api/Modules/AdminBillingModule.cs
@@ -1,12 +1,9 @@
-<<<<<<< Updated upstream
 ﻿using DataPlatform.Shared.Repositories;
 using Nancy;
 using Nancy.Responses.Negotiation;
 using Workflow.Billing.Domain.Entities;
-=======
-﻿using Nancy;
 using Shared.BuildingBlocks.Api.Security;
->>>>>>> Stashed changes
+
 
 namespace Billing.Api.Modules
 {
