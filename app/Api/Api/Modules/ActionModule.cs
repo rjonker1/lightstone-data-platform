b744e26e04c8b4a0ea40de393359a18be8149a9e
Allistaire Clark <allistairec@lightstone.co.za>
2015-05-18 13:04:30 +0200

    Api update

--- app/Api/Api/Modules/ActionModule.cs
+++ app/Api/Api/Modules/ActionModule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Api.Domain.Infrastructure.Dto;
 using DataPlatform.Shared.Dtos;
+using DataPlatform.Shared.Helpers.Extensions;
 using Nancy;
 using Nancy.Json;
 using Nancy.ModelBinding;
@@ -29,12 +30,15 @@ namespace Api.Modules
                 return Response.AsJson(metaData);
             };
 
-            Post["/action/LicensePlateNumberSearch"] = parameters =>
+            Post["/action"] = parameters =>
             {
+                this.Info(() => "Api action started.");
                 var apiRequest = this.Bind<ApiRequestDto>();
                 var token = Context.Request.Headers.Authorization.Split(' ')[1];
+                this.Info(() => "Api request: ContractId {0} Api token:{1}".FormatWith(apiRequest.ContractId, token));
                 var responses = packageBuilderApi.Post("", "/Packages/Execute", apiRequest, new[] { new KeyValuePair<string, string>("Authorization", "Token " + token), new KeyValuePair<string, string>("Content-Type", "application/json"), });
                 //var responses = packageBuilderApi.Get("", string.Format("/Packages/Execute/{0}/{1}/{2}/{3}", apiRequest.PackageId, apiRequest.UserId, apiRequest.SearchTerm, Guid.NewGuid()), null, new[] { new KeyValuePair<string, string>("Authorization", "Token " + token) });
+                this.Info(() => "Api action completed.");
                 return responses;
             };
         }
