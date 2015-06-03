2fb2ddc9c87389f53c5f04bbf8388aa56a7ebe2a
Allistaire Clark <allistairec@lightstone.co.za>
2015-06-03 15:02:06 +0200

    Added jQuery file upload basic functionality

--- app/UserManagement/UserManagement.Api/Modules/ClientModule.cs
+++ app/UserManagement/UserManagement.Api/Modules/ClientModule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using AutoMapper;
 using Nancy;
 using Nancy.ModelBinding;
@@ -38,6 +39,8 @@ namespace UserManagement.Api.Modules
             };
 
             Get["/Clients/Add"] = parameters => View["Save", new ClientDto()];
+
+            Get["/Clients/ImportUsers"] = _ => View["ImportClientUser"];
 
             Post["/Clients"] = _ =>
             {
