dc0f625f3ba4b6a4ad84e813b3bf90b413a0bd3e
Allistaire Clark <allistairec@lightstone.co.za>
2015-06-09 11:49:15 +0200

    PB Refs

--- app/PackageBuilder/PackageBuilder.Domain/CommandHandlers/DataProviders/Responses/LightstoneBusinessResponse.cs
+++ app/PackageBuilder/PackageBuilder.Domain/CommandHandlers/DataProviders/Responses/LightstoneBusinessResponse.cs
@@ -5,15 +5,15 @@ namespace PackageBuilder.Domain.CommandHandlers.DataProviders.Responses
 {
     public class LightstoneBusinessResponse
     {
-        private static Lace.Domain.Core.Entities.LightstoneBusinessResponse DefaultLightstoneBusinessResponse()
-        {
+        //private static Lace.Domain.Core.Entities.LightstoneBusinessResponse DefaultLightstoneBusinessResponse()
+        //{
 
-            var result = new List<IRespondWithBusiness>(); // List<ReturnCompaniesResponse.Company>();l
+        //    var result = new List<IRespondWithBusiness>(); // List<ReturnCompaniesResponse.Company>();l
 
-            return new Lace.Domain.Core.Entities.LightstoneBusinessResponse(result)
-            {
-                // TODO: new up a company response
-            };
-        } 
+        //    return new Lace.Domain.Core.Entities.LightstoneBusinessResponse(result)
+        //    {
+        //        // TODO: new up a company response
+        //    };
+        //} 
     }
 }
