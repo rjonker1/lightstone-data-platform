af95cdb8de9a8cef9144cc3a065deb7789c8fe02
Allistaire Clark <allistairec@lightstone.co.za>
2015-06-12 10:43:43 +0200

    Added ApiRequestValidator

---
+++ app/Api/Api/Helpers/Validators/ApiRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using Shared.BuildingBlocks.Api.ApiClients;
+
+namespace Api.Helpers.Validators
+{
+    public class ApiRequestValidator
+    {
+        private readonly IUserManagementApiClient _userManagementApiClient;
+
+        public ApiRequestValidator(IUserManagementApiClient userManagementApiClient)
+        {
+            _userManagementApiClient = userManagementApiClient;
+        }
+
+        public string AuthenticateRequest()
+        {
+            // TODO: Validate User
+            // TODO: Validate Customer | Client
+            // TODO: Validate Contract
+            // TODO: Validate Package
+        }
+    }
+}
