f953b858b9c2d12e5776d4bf6fc38edfbc9dc4f7
Rudi Jonker <rudi@customapp.co.za>
2015-06-30 19:00:19 +0200

    connection manager

---
+++ app/Lace/Source/Workflow.Lace/Persistence/PersistenceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Workflow.Lace.Persistence
+{
+    public sealed class PersistenceManager
+    {
+        private static readonly IDbConnection _connection = new SqlConnection(
+            ConfigurationManager.ConnectionStrings["workflow/dataprovider/database"]
+                .ConnectionString);
+
+        static PersistenceManager()
+        {
+            
+        }
+
+        public static IDbConnection Connection
+        {
+            get
+            {
+                return _connection;
+            }
+        }
+    }
+}
