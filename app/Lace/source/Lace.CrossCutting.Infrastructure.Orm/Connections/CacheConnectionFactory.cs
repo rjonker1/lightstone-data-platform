9926e8874358228d45923ae017f84156adc44771
Rudi Jonker <rudi@customapp.co.za>
2015-07-09 16:06:40 +0200

    lace - cache repository

--- app/Lace/source/Lace.CrossCutting.Infrastructure.Orm/Connections/CacheConnectionFactory.cs
+++ app/Lace/source/Lace.CrossCutting.Infrastructure.Orm/Connections/CacheConnectionFactory.cs
@@ -4,19 +4,37 @@ using ServiceStack.Redis;
 
 namespace Lace.CrossCutting.Infrastructure.Orm.Connections
 {
-    public class CacheConnectionFactory
+    public sealed class CacheConnectionFactory
     {
         private const string CacheIp = "127.0.0.1:6379";
+        private static readonly PooledRedisClientManager _redisClient;
 
-        private static readonly PooledRedisClientManager LocalClientManager =
-            new PooledRedisClientManager(10000, 1000, new[] {CacheIp})
+        public static PooledRedisClientManager LocalClient
+        {
+            get
             {
-                ConnectTimeout = 15000
-            };
-
-
-        public static Func<PooledRedisClientManager> LocalClient = () => LocalClientManager;
+                return _redisClient ?? new PooledRedisClientManager(10000, 1000, new[] { CacheIp })
+                {
+                    ConnectTimeout = 15000
+                };
+            }
+        }
     }
+
+
+    //public class CacheConnectionFactory
+    //{
+    //    private const string CacheIp = "127.0.0.1:6379";
+
+    //    private static readonly PooledRedisClientManager LocalClientManager =
+    //        new PooledRedisClientManager(10000, 1000, new[] {CacheIp})
+    //        {
+    //            ConnectTimeout = 15000
+    //        };
+
+
+    //    public static Func<PooledRedisClientManager> LocalClient = () => LocalClientManager;
+    //}
 
     public static class CacheConnectionExtenstions
     {
