de12f24a684e18f959a8bf7a3353390eca0c3d4d
Allistaire Clark <allistairec@lightstone.co.za>
2015-07-21 11:53:37 +0200

    PB Handler cleanup

--- app/PackageBuilder/PackageBuilder.Domain/CommandHandlers/Packages/UpdatePackageHandler.cs
+++ app/PackageBuilder/PackageBuilder.Domain/CommandHandlers/Packages/UpdatePackageHandler.cs
@@ -39,7 +39,6 @@ namespace PackageBuilder.Domain.CommandHandlers.Packages
                 command.CreatedDate, command.EditedDate, command.DataProviderValueOverrides);
 
             _writeRepo.Save(entity, Guid.NewGuid());
-            //_writeRepo.CacheSave(entity);
         }
     }
 }
