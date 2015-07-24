00a3673300571d2cd318b56c0f842960810909da
Allistaire Clark <allistairec@lightstone.co.za>
2015-07-24 12:55:39 +0200

    Billing - Unit test updates

--- app/Billing/Billing.Acceptance.Tests/Transaction/when_create_a_new_transaction.cs
+++ app/Billing/Billing.Acceptance.Tests/Transaction/when_create_a_new_transaction.cs
@@ -1,46 +1,46 @@
-﻿using Billing.Acceptance.Tests.Fixtures;
-using Billing.Api.Connector;
-using Billing.Api.Connector.Configuration;
-using Billing.Api.Dtos;
-using Billing.TestHelper.Mothers.CreatTransactionDto;
-using Xunit;
-using Xunit.Extensions;
+﻿//using Billing.Acceptance.Tests.Fixtures;
+//using Billing.Api.Connector;
+//using Billing.Api.Connector.Configuration;
+//using Billing.Api.Dtos;
+//using Billing.TestHelper.Mothers.CreatTransactionDto;
+//using Xunit;
+//using Xunit.Extensions;
 
-namespace Billing.Acceptance.Tests.Transaction
-{
-    public class when_create_a_new_transaction : Specification, IUseFixture<RebuildBillingEnvironment>
-    {
-        private readonly IConnectToBilling connector;
-        private readonly CreateTransaction transaction;
-        private BillingConnectorResponse response;
+//namespace Billing.Acceptance.Tests.Transaction
+//{
+//    public class when_create_a_new_transaction : Specification, IUseFixture<RebuildBillingEnvironment>
+//    {
+//        private readonly IConnectToBilling connector;
+//        private readonly CreateTransaction transaction;
+//        private BillingConnectorResponse response;
 
-        public when_create_a_new_transaction()
-        {
-            connector = new DefaultBillingConnector(new ApplicationConfigurationBillingConnectorConfiguration());
-            transaction = new CreateTransactionMother().DefaultCreateTransaction();
-        }
+//        public when_create_a_new_transaction()
+//        {
+//            connector = new DefaultBillingConnector(new ApplicationConfigurationBillingConnectorConfiguration());
+//            transaction = new CreateTransactionMother().DefaultCreateTransaction();
+//        }
 
-        public override void Observe()
-        {
-            response = connector.CreateTransaction(transaction);
-        }
+//        public override void Observe()
+//        {
+//            response = connector.CreateTransaction(transaction);
+//        }
 
-        [Observation]
-        public void the_api_accepts_the_transaction()
-        {
-            response.Success.ShouldBeTrue();
-        }
+//        [Observation]
+//        public void the_api_accepts_the_transaction()
+//        {
+//            response.Success.ShouldBeTrue();
+//        }
 
-        [Observation]
-        public void a_transaction_is_created()
-        {
+//        [Observation]
+//        public void a_transaction_is_created()
+//        {
             
-        }
+//        }
 
 
-        public void SetFixture(RebuildBillingEnvironment environmentSetup)
-        {
-            environmentSetup.Run();
-        }
-    }
-}
+//        public void SetFixture(RebuildBillingEnvironment environmentSetup)
+//        {
+//            environmentSetup.Run();
+//        }
+//    }
+//}
--- app/Billing/Billing.Unit.Tests/Api/Transaction/when_creating_a_new_billing_transaction_as_an_unauthorised_user.cs
+++ app/Billing/Billing.Unit.Tests/Api/Transaction/when_creating_a_new_billing_transaction_as_an_unauthorised_user.cs
@@ -1,33 +1,33 @@
-﻿using Billing.Api.Dtos;
-using Billing.TestHelper.Fakes;
-using Billing.TestHelper.Mothers.CreatTransactionDto;
-using Nancy;
-using Nancy.Testing;
-using Xunit.Extensions;
+﻿//using Billing.Api.Dtos;
+//using Billing.TestHelper.Fakes;
+//using Billing.TestHelper.Mothers.CreatTransactionDto;
+//using Nancy;
+//using Nancy.Testing;
+//using Xunit.Extensions;
 
-namespace Billing.Api.Tests.Api.Transaction
-{
-    public class when_creating_a_new_billing_transaction_as_an_unauthorised_user : TransactionModuleTestBase, AsUnautorisedUser
-    {
-        private readonly CreateTransaction transaction;
-        private BrowserResponse result;
+//namespace Billing.Api.Tests.Api.Transaction
+//{
+//    public class when_creating_a_new_billing_transaction_as_an_unauthorised_user : TransactionModuleTestBase, AsUnautorisedUser
+//    {
+//        private readonly CreateTransaction transaction;
+//        private BrowserResponse result;
 
-        public when_creating_a_new_billing_transaction_as_an_unauthorised_user()
-            : base(new NullPublisher())
-        {
-            transaction = new CreateTransactionMother().DefaultCreateTransaction();
-        }
+//        public when_creating_a_new_billing_transaction_as_an_unauthorised_user()
+//            : base(new NullPublisher())
+//        {
+//            transaction = new CreateTransactionMother().DefaultCreateTransaction();
+//        }
 
-        public override void Observe()
-        {
-            result = Post(with => with.JsonBody(transaction));
-        }
+//        public override void Observe()
+//        {
+//            result = Post(with => with.JsonBody(transaction));
+//        }
 
-        [Observation]
-        public void an_unautorised_error_is_returned()
-        {
-            result.StatusCode.ShouldEqual(HttpStatusCode.Unauthorized);
-        }
+//        [Observation]
+//        public void an_unautorised_error_is_returned()
+//        {
+//            result.StatusCode.ShouldEqual(HttpStatusCode.Unauthorized);
+//        }
 
-    }
-}
+//    }
+//}
