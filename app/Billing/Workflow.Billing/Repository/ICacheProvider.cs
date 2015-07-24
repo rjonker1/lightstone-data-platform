43373ae4117042da67cab152452addb8d2111a7f
Allistaire Clark <allistairec@lightstone.co.za>
2015-07-24 14:46:09 +0200

    Billing - CacheProvider

---
+++ app/Billing/Workflow.Billing/Repository/ICacheProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Workflow.Billing.Repository
+{
+    public interface ICacheProvider<T>
+    {
+        T CacheGet(Guid entityId);
+        void CacheSave(T entity);
+        void CacheDelete(Guid entityId);
+    }
+}
