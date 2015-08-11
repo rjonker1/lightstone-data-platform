a92a30ed058b5311827d6045edd4c053005d5ed8
Allistaire Clark <allistairec@lightstone.co.za>
2015-08-11 14:11:00 +0200

    Billing - BillingRun Consumer config and setup

---
+++ app/Billing/Workflow.Billing.BillRun.Consumer/BillingRunService.cs
@@ -0,0 +1,47 @@
+﻿using Castle.Windsor;
+using Common.Logging;
+using DataPlatform.Shared.Messaging.Billing.Messages.BillingRun;
+using EasyNetQ;
+using Workflow.Billing.Consumers;
+using Workflow.Billing.Consumers.Installers;
+using Workflow.Billing.Installers;
+
+namespace Workflow.Billing.BillingRun.Consumer
+{
+    public class BillingRunService : IBillingRunService
+    {
+        private readonly ILog _log = LogManager.GetLogger<BillingRunService>();
+        private IAdvancedBus advancedBus;
+
+        public void Start()
+        {
+            _log.DebugFormat("Started billing cache service");
+
+            var container = new WindsorContainer().Install(
+                new NHibernateInstaller(),
+                new WindsorInstaller(),
+                new CacheProviderInstaller(),
+                new RepositoryInstaller(),
+                new ConsumerInstaller(),
+                new BusInstaller());
+
+            advancedBus = container.Resolve<IAdvancedBus>();
+            var cache = advancedBus.QueueDeclare("DataPlatform.BillingRun");
+
+            advancedBus.Consume(cache, x => x
+                .Add<BillingMessage>((message, info) => new TransactionConsumer<BillingMessage>(message, container)));
+
+            _log.DebugFormat("Billing cache service started");
+        }
+
+        public void Stop()
+        {
+            if (advancedBus != null)
+            {
+                advancedBus.Dispose();
+            }
+
+            _log.DebugFormat("Stopped billing cache service");
+        }
+    }
+}
--- app/Billing/Workflow.Billing.Consumer/BillingService.cs
+++ app/Billing/Workflow.Billing.Consumer/BillingService.cs
@@ -1,12 +1,10 @@
 ﻿using Castle.Windsor;
 using Common.Logging;
 using DataPlatform.Shared.Messaging.Billing.Messages;
-using DataPlatform.Shared.Messaging.Billing.Messages.BillingRun;
 using EasyNetQ;
 using Workflow.Billing.Consumers;
 using Workflow.Billing.Consumers.Installers;
 using Workflow.Billing.Installers;
-using Workflow.Billing.Messages.Publishable;
 
 namespace Workflow.Billing.Consumer
 {
@@ -30,24 +28,16 @@ namespace Workflow.Billing.Consumer
                 new PublishReportQueueInstaller(),
                 new PivotInstaller());
 
-            //bus = container.Resolve<IBus>();
             advancedBus = container.Resolve<IAdvancedBus>();
             var q = advancedBus.QueueDeclare("DataPlatform.Transactions.Billing");
-            //var cache = advancedBus.QueueDeclare("DataPlatform.Cache.Billing");
-
-            //advancedBus.Consume(q, x => container.Resolve(typeof(IConsume<>)));
-            //advancedBus.Consume(q, x => container.Resolve<TransactionConsumer>());
+            
             advancedBus.Consume(q, x => x
-                .Add<BillingMessage>((message, info) => new TransactionConsumer<BillingMessage>(message, container))
                 .Add<InvoiceTransactionCreated>((message, info) => new TransactionConsumer<InvoiceTransactionCreated> (message, container))
                 .Add<UserMessage>((message, info) => new TransactionConsumer<UserMessage> (message, container))
                 .Add<CustomerMessage>((message, info) => new TransactionConsumer<CustomerMessage>(message, container))
                 .Add<ClientMessage>((message, info) => new TransactionConsumer<ClientMessage>(message, container))
                 .Add<PackageMessage>((message, info) => new TransactionConsumer<PackageMessage>(message, container))
                 .Add<ContractMessage>((message, info) => new TransactionConsumer<ContractMessage>(message, container)));
-
-            //advancedBus.Consume(cache, x => x
-            //    .Add<BillCacheMessage>((message, info) => new TransactionConsumer<BillCacheMessage>(message, container)));
 
             _log.DebugFormat("Billing service started");
         }
