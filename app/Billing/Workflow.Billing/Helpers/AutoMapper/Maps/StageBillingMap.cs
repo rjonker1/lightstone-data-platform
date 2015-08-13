350b608cfd5d7b3b86d11c62e0f53f070b1f2667
Allistaire Clark <allistairec@lightstone.co.za>
2015-08-13 15:06:58 +0200

    Billing - StageBillingMap amendments

--- app/Billing/Workflow.Billing/Helpers/AutoMapper/Maps/StageBillingMap.cs
+++ app/Billing/Workflow.Billing/Helpers/AutoMapper/Maps/StageBillingMap.cs
@@ -12,10 +12,10 @@ namespace Workflow.Billing.Helpers.AutoMapper.Maps
             Mapper.CreateMap<PreBilling, StageBilling>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(s => Guid.NewGuid()))
                 .ForMember(dest => dest.PreBillingId, opt => opt.MapFrom(s => s.Id))
-                .ForMember(dest => dest.User, opt => opt.MapFrom(s => s))
-                .ForMember(dest => dest.UserTransaction, opt => opt.MapFrom(s => s))
-                .AfterMap((src, dest) => dest.User.HasTransactions = true)
-                .AfterMap((src, dest) => dest.UserTransaction.IsBillable = true);
+                //.ForMember(dest => dest.User.HasTransactions, opt => opt.MapFrom(s => s))
+                //.ForMember(dest => dest.UserTransaction.IsBillable, opt => opt.MapFrom(s => s));
+            .AfterMap((src, dest) => dest.User.HasTransactions = true)
+            .AfterMap((src, dest) => dest.UserTransaction.IsBillable = true);
         }
     }
 }
