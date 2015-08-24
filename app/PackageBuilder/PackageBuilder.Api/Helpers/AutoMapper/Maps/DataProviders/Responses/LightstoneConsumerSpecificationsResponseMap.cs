49732627f6abcb1dac9d700adcb17de251581c97
Rudi Jonker <rudi@customapp.co.za>
2015-08-24 19:18:36 +0200

    package builder - update lace references

--- app/PackageBuilder/PackageBuilder.Api/Helpers/AutoMapper/Maps/DataProviders/Responses/LightstoneConsumerSpecificationsResponseMap.cs
+++ app/PackageBuilder/PackageBuilder.Api/Helpers/AutoMapper/Maps/DataProviders/Responses/LightstoneConsumerSpecificationsResponseMap.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using AutoMapper;
 using Lace.Domain.Core.Contracts.DataProviders;
+using Lace.Domain.Core.Contracts.DataProviders.Consumer;
 using PackageBuilder.Domain.Entities.DataFields.Write;
 
 namespace PackageBuilder.Api.Helpers.AutoMapper.Maps.DataProviders.Responses
@@ -11,6 +15,15 @@ namespace PackageBuilder.Api.Helpers.AutoMapper.Maps.DataProviders.Responses
         {
             Mapper.CreateMap<IProvideDataFromLightstoneConsumerSpecifications, IEnumerable<DataField>>()
              .ConvertUsing(Mapper.Map<object, IEnumerable<DataField>>);
+
+            Mapper.CreateMap<IEnumerable<IRespondWithRepairData>, DataField>()
+               .ForMember(d => d.Type, opt => opt.MapFrom(x => x.GetType()))
+               .ForMember(d => d.DataFields, opt => opt.MapFrom(SourceMember<IRespondWithRepairData>()));
+        }
+
+        private static Expression<Func<IEnumerable<T>, IEnumerable<DataField>>> SourceMember<T>()
+        {
+            return x => x != null ? x.SelectMany(arg => Mapper.Map<object, IEnumerable<DataField>>(arg)).ToList() : Enumerable.Empty<DataField>();
         }
     }
 }
