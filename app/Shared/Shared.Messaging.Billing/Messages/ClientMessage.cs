7e5bdf34166b47a75faf28ebc3bf85aba8be7f7a
Allistaire Clark <allistairec@lightstone.co.za>
2015-09-07 10:11:20 +0200

    Modified Customer | Client shared messages to accomodate for PaymentType for debit order

--- app/Shared/Shared.Messaging.Billing/Messages/ClientMessage.cs
+++ app/Shared/Shared.Messaging.Billing/Messages/ClientMessage.cs
@@ -10,6 +10,7 @@ namespace DataPlatform.Shared.Messaging.Billing.Messages
         public virtual string AccountNumber { get; set; }
         public virtual string AccountOwner { get; set; }
         public virtual string BillingType { get; set; }
+        public virtual string PaymentType { get; set; }
         public virtual Guid ClientId { get; set; }
         public virtual string ClientName { get; set; }
 
