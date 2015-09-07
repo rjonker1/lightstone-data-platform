0ea019f062d3657c3e6b9158872e4f5768f5e806
Allistaire Clark <allistairec@lightstone.co.za>
2015-09-07 18:46:31 +0200

    Added BillDateTime fields to ReportData entity for output files

--- app/Reporting/Workflow.Reporting/Entities/ReportData.cs
+++ app/Reporting/Workflow.Reporting/Entities/ReportData.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Workflow.Reporting.Entities
 {
     public class ReportData
     {
         public ReportCustomer Customer { get; set; }
+        public DateTime BillDateTime1 { get; set; }
+        public DateTime BillDateTime2 { get; set; }
         public IEnumerable<ReportInvoice> Invoices { get; set; }
         public IEnumerable<PreBillingRecord> PreBillingData { get; set; }
         public IEnumerable<ReportDebitOrder> DebitOrders { get; set; } 
