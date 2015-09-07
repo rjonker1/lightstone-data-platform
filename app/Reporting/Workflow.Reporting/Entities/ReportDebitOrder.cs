663972421d532f4b4e98aa77bf82ed00dce78a67
Allistaire Clark <allistairec@lightstone.co.za>
2015-09-07 11:53:38 +0200

    Added ReportDebitOrder entity to Reporting

---
+++ app/Reporting/Workflow.Reporting/Entities/ReportDebitOrder.cs
@@ -0,0 +1,14 @@
+﻿namespace Workflow.Reporting.Entities
+{
+    public class ReportDebitOrder
+    {
+        public string PastelAccountId { get; set; }
+        public string AccountName { get; set; }
+        public string BankAccountName { get; set; }
+        public string AccountType { get; set; }
+        public string BranchCode { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string ContractAmount { get; set; }
+        public string BatchAmount { get; set; }
+    }
+}
