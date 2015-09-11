aea9681ea5c132762f136bd323182ae6a5b7b439
Allistaire Clark <allistairec@lightstone.co.za>
2015-09-11 21:44:11 +0200

    Cradle Nancy Self Hosted

--- app/Cradle/Cradle.KeepAlive.Service/Program.cs
+++ app/Cradle/Cradle.KeepAlive.Service/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
 
 namespace Cradle.KeepAlive.Service
 {
@@ -10,6 +7,14 @@ namespace Cradle.KeepAlive.Service
     {
         static void Main(string[] args)
         {
+            var url = "http://+:8080";
+
+            using (WebApp.Start<StartupBootstrapper>(url))
+            {
+                Console.WriteLine("Running on {0}", url);
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ app/Cradle/Cradle.KeepAlive.Service/StartupBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Cradle.KeepAlive.Service.StartupBootstrapper))]
+
+namespace Cradle.KeepAlive.Service
+{
+    public class StartupBootstrapper
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
