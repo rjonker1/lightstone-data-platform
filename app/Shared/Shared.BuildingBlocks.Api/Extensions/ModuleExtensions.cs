77a5da7df97bde62718b456534ee659f90ad79f0
bakerbrian7 <brian@customapp.co.za>
2015-09-16 08:48:12 +0200

    Added claims per request extensions

---
+++ app/Shared/Shared.BuildingBlocks.Api/Extensions/ModuleExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using Nancy.Security;
+using Shared.BuildingBlocks.Api.Security;
+
+namespace Shared.BuildingBlocks.Api.Extensions
+{
+    public static class ModuleExtensions
+    {
+        public static void RequiresRequestClaims(this INancyModule module, params string[] claims)
+        {
+            module.After.AddItemToEndOfPipeline(ModuleHooks.ReturnForbiddenResponse());
+            module.RequiresClaims(claims);
+        }
+    }
+}
---
+++ app/Shared/Shared.BuildingBlocks.Api/Security/ModuleHooks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Nancy;
+using Shared.BuildingBlocks.Api.ExceptionHandling;
+
+namespace Shared.BuildingBlocks.Api.Security
+{
+    public class ModuleHooks
+    {
+        public static Action<NancyContext> ReturnForbiddenResponse()
+        {
+            return context =>
+            {
+                if (context.Response.StatusCode == HttpStatusCode.Forbidden)
+                    context.Response = ErrorResponse.FromException(new UnauthorizedAccessException()).WithStatusCode(HttpStatusCode.Forbidden);
+            };
+        }
+    }
+}
