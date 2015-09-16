9b986f2b8f1bfff765890cf269679dfa55fe6385
Allistaire Clark <allistairec@lightstone.co.za>
2015-09-16 09:10:41 +0200

    Added Maintenance response to API

--- app/Api/Api/Modules/AuthModule.cs
+++ app/Api/Api/Modules/AuthModule.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using Nancy;
 using Shared.BuildingBlocks.Api.ApiClients;
@@ -11,6 +13,8 @@ namespace Api.Modules
         {
             Post["/login"] = parameters =>
             {
+                if (Convert.ToBoolean(ConfigurationManager.AppSettings["maintenanceMode"])) return Response.AsJson(new { data = "Service unavailable - Maintenance in progress" }, HttpStatusCode.ServiceUnavailable);
+
                 var username = Context.Request.Headers["Username"].FirstOrDefault();
                 var password = Context.Request.Headers["Password"].FirstOrDefault();
 
