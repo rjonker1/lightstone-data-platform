35d6dede2f4f669ec10e79d3eaa198b9fedc8b9f
Allistaire Clark <allistairec@lightstone.co.za>
2015-09-21 12:28:46 +0200

    MMCodeDataProvider

---
+++ app/Lace/Source/Lace.Domain.Core.Entities/MMCodeResponse.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Lace.Domain.Core.Contracts.DataProviders;
+
+namespace Lace.Domain.Core.Entities
+{
+    [DataContract]
+    public class MMCodeResponse : IProvideDataFromMMCode
+    {
+        public MMCodeResponse()
+        {
+        }
+
+        public static MMCodeResponse Empty()
+        {
+            return new MMCodeResponse();
+        }
+
+        public MMCodeResponse(int mmlId, int carId, string mmCode)
+        {
+            MMLId = mmlId;
+            CarId = carId;
+            MMCode = mmCode;
+        }
+
+        [DataMember]
+        public int MMLId { get; set; }
+        [DataMember]
+        public int CarId { get; set; }
+        [DataMember]
+        public string MMCode { get; set; }
+
+        [DataMember]
+        public string TypeName
+        {
+            get
+            {
+                return GetType().Name;
+            }
+        }
+        [DataMember]
+        public Type Type
+        {
+            get
+            {
+                return GetType();
+            }
+        }
+
+        [DataMember]
+        public bool Handled { get; private set; }
+
+        public void HasNotBeenHandled()
+        {
+            Handled = false;
+        }
+
+        public void HasBeenHandled()
+        {
+            Handled = true;
+        }
+    }
+}
---
+++ app/Lace/Source/Lace.Domain.Core/Contracts/DataProviders/IProvideDataFromMMCode.cs
@@ -0,0 +1,11 @@
+﻿using Lace.Domain.Core.Contracts.Requests;
+
+namespace Lace.Domain.Core.Contracts.DataProviders
+{
+    public interface IProvideDataFromMMCode : IPointToLaceProvider
+    {
+        int MMLId { get; set; }
+        int CarId { get; set; }
+        string MMCode { get; set; }
+    }
+}
