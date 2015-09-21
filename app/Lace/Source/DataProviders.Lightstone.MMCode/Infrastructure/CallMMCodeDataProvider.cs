20c47d5988a84b0eaf89b106bd87e69860df3d55
Allistaire Clark <allistairec@lightstone.co.za>
2015-09-21 12:28:46 +0200

    MMCodeDataProvider

---
+++ app/Lace/Source/DataProviders.Lightstone.MMCode/Infrastructure/CallMMCodeDataProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Common.Logging;
+using Lace.Domain.Core.Contracts.Requests;
+using Lace.Domain.Core.Requests.Contracts;
+using Lace.Domain.DataProviders.Core.Contracts;
+using Lace.Toolbox.Database.Repositories;
+
+namespace DataProviders.Lightstone.MMCode.Infrastructure
+{
+    public class CallMMCodeDataProvider : ICallTheDataProviderSource
+    {
+        private readonly ILog _log;
+        private readonly IAmDataProvider _dataProvider;
+        private readonly ILogCommandTypes _logCommand;
+
+        private readonly IReadOnlyRepository _repository;
+
+        public CallMMCodeDataProvider(IAmDataProvider dataProvider, IReadOnlyRepository repository, ILogCommandTypes logCommand)
+        {
+            _log = LogManager.GetLogger(GetType());
+            _dataProvider = dataProvider;
+            _repository = repository;
+            _logCommand = logCommand;
+        }
+
+        public void CallTheDataProvider(ICollection<IPointToLaceProvider> response)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TransformResponse(ICollection<IPointToLaceProvider> response)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ app/Lace/Source/DataProviders.Lightstone.MMCode/Infrastructure/HandleMMCodeDataProviderCall.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Lace.Domain.DataProviders.Core.Contracts;
+
+namespace DataProviders.Lightstone.MMCode.Infrastructure
+{
+    public sealed class HandleMMCodeDataProviderCall : IHandleDataProviderSourceCall
+    {
+        public void Request(Action<IRequestDataFromDataProviderSource> action)
+        {
+            action(new RequestDataFromMMCodeSource());
+        }
+    }
+}
