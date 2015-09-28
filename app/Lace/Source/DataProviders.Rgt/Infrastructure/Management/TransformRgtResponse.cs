3ef06f30d42974700bbef7982ef4388290daaf64
Allistaire Clark <allistairec@lightstone.co.za>
2015-09-28 13:28:38 +0200

    LACE - Specs transformation & response

--- app/Lace/Source/DataProviders.Rgt/Infrastructure/Management/TransformRgtResponse.cs
+++ app/Lace/Source/DataProviders.Rgt/Infrastructure/Management/TransformRgtResponse.cs
@@ -28,7 +28,14 @@ namespace Lace.Domain.DataProviders.Rgt.Infrastructure.Management
             Result = new RgtResponse(_carSpecification.ManufacturerName, _carSpecification.ModelYear.HasValue ? _carSpecification.ModelYear.Value : 0,_carSpecification.CarTypeName,
                 _carSpecification.TopSpeed, _carSpecification.Kilowatts,_carSpecification.FuelEconomy,_carSpecification.Acceleration,_carSpecification.Torque,_carSpecification.Emissions,_carSpecification.EngineSize,
                 _carSpecification.BodyShape,_carSpecification.FuelType,_carSpecification.TransmissionType,_carSpecification.CarFullname,_carSpecification.Colour,_carSpecification.RainSensorWindscreenWipers,_carSpecification.HeadUpDisplay,
-                _carSpecification.VehicleType,_carSpecification.CarModel,string.Empty,_carSpecification.CarType);
+                _carSpecification.VehicleType,_carSpecification.CarModel,string.Empty,_carSpecification.CarType, _carSpecification.AirConditioner, _carSpecification.ElectricMirrors, 
+                _carSpecification.FoldAwayMirrors, _carSpecification.HeatedSideMirrors, _carSpecification.Doors, _carSpecification.BoreXStroke, _carSpecification.CompressionRatio, 
+                _carSpecification.Cylinders, _carSpecification.ValvesPerCylinder, _carSpecification.ABSBrakes, _carSpecification.BrakesFrontDiscs, _carSpecification.BrakesRearDiscs, 
+                _carSpecification.FogLampsFront, _carSpecification.HeadlightType, _carSpecification.HeatedRearWindow, _carSpecification.RearWiper, _carSpecification.PowerSteering, 
+                _carSpecification.ColourCodedBumpers, _carSpecification.ColourCodedDoorHandles, _carSpecification.ColourCodedMirrors, _carSpecification.MaintenancePlanKms, 
+                _carSpecification.MaintenancePlanYears, _carSpecification.ServiceIntervalsKms, _carSpecification.ServicePlanKms, _carSpecification.ServicePlanYears, 
+                _carSpecification.WarrantyKms, _carSpecification.WarrantyYears, _carSpecification.RimSizeFront, _carSpecification.TyreSizeFront, _carSpecification.TyreSizeRear,
+                _carSpecification.WheelType);
         }
     }
 }
--- app/Lace/Source/Lace.Domain.Core.Entities/RgtResponse.cs
+++ app/Lace/Source/Lace.Domain.Core.Entities/RgtResponse.cs
@@ -28,11 +28,46 @@ namespace Lace.Domain.Core.Entities
             return new RgtResponse(message);
         }
 
-        public RgtResponse(string manufacturer, int modelYear, string modelType, string topSpeed, string kilowatts, string fuelEconomy,
-            string acceleration, string torque, string emissions,
-            string engineSize, string bodyShape, string fuelType, string transmissionType, string carFullName,
-            string colour, string rainSensorWipers, string headsUpDisplay,
-            string vehicleType, string model, string make, string carType)
+        //public RgtResponse(string manufacturer, int modelYear, string modelType, string topSpeed, string kilowatts, string fuelEconomy,
+        //    string acceleration, string torque, string emissions,
+        //    string engineSize, string bodyShape, string fuelType, string transmissionType, string carFullName,
+        //    string colour, string rainSensorWipers, string headsUpDisplay,
+        //    string vehicleType, string model, string make, string carType)
+        //{
+        //    Manufacturer = manufacturer;
+        //    ModelYear = modelYear;
+        //    ModelType = modelType;
+        //    TopSpeed = topSpeed;
+        //    Kilowatts = kilowatts;
+        //    FuelEconomy = fuelEconomy;
+        //    Acceleration = acceleration;
+        //    Torque = torque;
+        //    Emissions = emissions;
+        //    EngineSize = engineSize;
+        //    BodyShape = bodyShape;
+        //    FuelType = fuelType;
+        //    TransmissionType = transmissionType;
+        //    CarFullname = carFullName;
+        //    Colour = colour;
+        //    RainSensorWindscreenWipers = rainSensorWipers;
+        //    HeadUpDisplay = headsUpDisplay;
+        //    VehicleType = vehicleType;
+        //    Model = model;
+        //    Make = make;
+        //    CarType = carType;
+        //}
+
+        public RgtResponse(string manufacturer, int modelYear, string modelType, string topSpeed, string kilowatts, string fuelEconomy, 
+            string acceleration, string torque, string emissions, string engineSize, string bodyShape, string fuelType, 
+            string transmissionType, string carFullname, string colour, string rainSensorWindscreenWipers, string headUpDisplay,
+            string vehicleType, string model, string make, string carType, string airConditioner, string electricMirrors, 
+            string foldAwayMirrors, string heatedSideMirrors, string doors, string boreXStroke, string compressionRatio, 
+            string cylinders, string valvesPerCylinder, string absBrakes, string brakesFrontDiscs, string brakesRearDiscs, 
+            string fogLampsFront, string headlightType, string heatedRearWindow, string rearWiper, string powerSteering, 
+            string colourCodedBumpers, string colourCodedDoorHandles, string colourCodedMirrors, string maintenancePlanKms, 
+            string maintenancePlanYears, string serviceIntervalsKms, string servicePlanKms, string servicePlanYears, 
+            string warrantyKms, string warrantyYears, string rimSizeFront, string tyreSizeFront, string tyreSizeRear, 
+            string wheelType)
         {
             Manufacturer = manufacturer;
             ModelYear = modelYear;
@@ -47,14 +82,45 @@ namespace Lace.Domain.Core.Entities
             BodyShape = bodyShape;
             FuelType = fuelType;
             TransmissionType = transmissionType;
-            CarFullname = carFullName;
+            CarFullname = carFullname;
             Colour = colour;
-            RainSensorWindscreenWipers = rainSensorWipers;
-            HeadUpDisplay = headsUpDisplay;
+            RainSensorWindscreenWipers = rainSensorWindscreenWipers;
+            HeadUpDisplay = headUpDisplay;
             VehicleType = vehicleType;
             Model = model;
             Make = make;
             CarType = carType;
+            AirConditioner = airConditioner;
+            ElectricMirrors = electricMirrors;
+            FoldAwayMirrors = foldAwayMirrors;
+            HeatedSideMirrors = heatedSideMirrors;
+            Doors = doors;
+            BoreXStroke = boreXStroke;
+            CompressionRatio = compressionRatio;
+            Cylinders = cylinders;
+            ValvesPerCylinder = valvesPerCylinder;
+            ABSBrakes = absBrakes;
+            BrakesFrontDiscs = brakesFrontDiscs;
+            BrakesRearDiscs = brakesRearDiscs;
+            FogLampsFront = fogLampsFront;
+            HeadlightType = headlightType;
+            HeatedRearWindow = heatedRearWindow;
+            RearWiper = rearWiper;
+            PowerSteering = powerSteering;
+            ColourCodedBumpers = colourCodedBumpers;
+            ColourCodedDoorHandles = colourCodedDoorHandles;
+            ColourCodedMirrors = colourCodedMirrors;
+            MaintenancePlanKms = maintenancePlanKms;
+            MaintenancePlanYears = maintenancePlanYears;
+            ServiceIntervalsKms = serviceIntervalsKms;
+            ServicePlanKms = servicePlanKms;
+            ServicePlanYears = servicePlanYears;
+            WarrantyKms = warrantyKms;
+            WarrantyYears = warrantyYears;
+            RimSizeFront = rimSizeFront;
+            TyreSizeFront = tyreSizeFront;
+            TyreSizeRear = tyreSizeRear;
+            WheelType = wheelType;
         }
 
         [DataMember]
@@ -122,6 +188,99 @@ namespace Lace.Domain.Core.Entities
 
         [DataMember]
         public string CarType { get; private set; }
+
+        [DataMember]
+        public string AirConditioner { get; private set; }
+
+        [DataMember]
+        public string ElectricMirrors { get; private set; }
+
+        [DataMember]
+        public string FoldAwayMirrors { get; private set; }
+
+        [DataMember]
+        public string HeatedSideMirrors { get; private set; }
+
+        [DataMember]
+        public string Doors { get; private set; }
+
+        [DataMember]
+        public string BoreXStroke { get; private set; }
+
+        [DataMember]
+        public string CompressionRatio { get; private set; }
+
+        [DataMember]
+        public string Cylinders { get; private set; }
+
+        [DataMember]
+        public string ValvesPerCylinder { get; private set; }
+
+        [DataMember]
+        public string ABSBrakes { get; private set; }
+
+        [DataMember]
+        public string BrakesFrontDiscs { get; private set; }
+
+        [DataMember]
+        public string BrakesRearDiscs { get; private set; }
+
+        [DataMember]
+        public string FogLampsFront { get; private set; }
+
+        [DataMember]
+        public string HeadlightType { get; private set; }
+
+        [DataMember]
+        public string HeatedRearWindow { get; private set; }
+
+        [DataMember]
+        public string RearWiper { get; private set; }
+
+        [DataMember]
+        public string PowerSteering { get; private set; }
+
+        [DataMember]
+        public string ColourCodedBumpers { get; private set; }
+
+        [DataMember]
+        public string ColourCodedDoorHandles { get; private set; }
+
+        [DataMember]
+        public string ColourCodedMirrors { get; private set; }
+
+        [DataMember]
+        public string MaintenancePlanKms { get; private set; }
+
+        [DataMember]
+        public string MaintenancePlanYears { get; private set; }
+
+        [DataMember]
+        public string ServiceIntervalsKms { get; private set; }
+
+        [DataMember]
+        public string ServicePlanKms { get; private set; }
+
+        [DataMember]
+        public string ServicePlanYears { get; private set; }
+
+        [DataMember]
+        public string WarrantyKms { get; private set; }
+
+        [DataMember]
+        public string WarrantyYears { get; private set; }
+
+        [DataMember]
+        public string RimSizeFront { get; private set; }
+
+        [DataMember]
+        public string TyreSizeFront { get; private set; }
+
+        [DataMember]
+        public string TyreSizeRear { get; private set; }
+
+        [DataMember]
+        public string WheelType { get; private set; }
 
         [DataMember]
         public string TypeName
