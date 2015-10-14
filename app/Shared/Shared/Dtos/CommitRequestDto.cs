5ae6fcd730181cd1e4433d54609dd83c94d25884
Allistaire Clark <allistairec@lightstone.co.za>
2015-10-14 16:23:50 +0200

    Shared - Added CommitRequestDto for request mapping

---
+++ app/Shared/Shared/Dtos/CommitRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+using DataPlatform.Shared.Enums;
+
+namespace DataPlatform.Shared.Dtos
+{
+    [DataContract]
+    public class CommitRequestDto
+    {
+        [DataMember]
+        public string CarId { get; set; }
+        [DataMember]
+        public string Year { get; set; }
+        [DataMember]
+        public Vin12State State { get; set; }
+    }
+}
