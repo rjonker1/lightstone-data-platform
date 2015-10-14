73478988c997edf77322ed2357de89b3efb7b063
Allistaire Clark <allistairec@lightstone.co.za>
2015-10-14 13:14:49 +0200

    PB - Added RequestAudit entity. Updated Shared refs

---
+++ app/PackageBuilder/PackageBuilder.Domain.Entities/Requests/Read/RequestAudit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DataPlatform.Shared.Enums;
+using PackageBuilder.Core.Entities;
+
+namespace PackageBuilder.Domain.Entities.Requests.Read
+{
+    public class RequestAudit : Entity
+    {
+        public virtual Guid RequestId { get; set; }
+        public virtual Vin12State State { get; set; }
+        public virtual DateTime RequestExpiration { get; set; }
+    }
+}
