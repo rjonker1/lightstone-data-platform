f609f7c21743786ed6e1b31423d0a15a6f2da9f8
Allistaire Clark <allistairec@lightstone.co.za>
2015-10-21 13:25:06 +0200

    Added TransactionRequestConsumer base

---
+++ app/Billing/Workflow.Billing.Consumers/ConsumerTypes/TransactionRequestConsumer.cs
@@ -0,0 +1,22 @@
+﻿using DataPlatform.Shared.Messaging.Billing.Messages;
+using DataPlatform.Shared.Repositories;
+using EasyNetQ;
+using Workflow.Billing.Domain.Entities;
+
+namespace Workflow.Billing.Consumers.ConsumerTypes
+{
+    public class TransactionRequestConsumer : IConsumeMessages<TransactionRequestMessage>
+    {
+        private readonly IRepository<TransactionRequest> _transactionRequestRepository;
+
+        public TransactionRequestConsumer(IRepository<TransactionRequest> transactionRequestRepository)
+        {
+            _transactionRequestRepository = transactionRequestRepository;
+        }
+
+        public void Consume(IMessage<TransactionRequestMessage> message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
