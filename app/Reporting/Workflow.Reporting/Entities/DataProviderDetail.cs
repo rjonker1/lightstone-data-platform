b12e96a2c449c79f1041dfcaa9eb57ee8aab9a62
Allistaire Clark <allistairec@lightstone.co.za>
2015-10-28 12:55:21 +0200

    Reporting - Added ResponseState & TransactionState to Reporting DataProviderDetails

--- app/Reporting/Workflow.Reporting/Entities/DataProviderDetail.cs
+++ app/Reporting/Workflow.Reporting/Entities/DataProviderDetail.cs
@@ -5,6 +5,8 @@
         public string DataProviderId { get; set; }
         public string DataProviderName { get; set; }
         public string CostPrice { get; set; }
-        public string RecommendedPrice { get; set; } 
+        public string RecommendedPrice { get; set; }
+        public virtual string ResponseState { get; set; }
+        public virtual string TransactionState { get; set; }
     }
 }
