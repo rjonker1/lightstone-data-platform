8a0182b0889eda5a9d005f7202fc43e7a5fc86e9
bakerbrian7 <brian@customapp.co.za>
2015-11-06 11:18:32 +0200

    Moved auth installer to shared

---
+++ app/Shared/Shared.BuildingBlocks.Api/Installers/AuthInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Nancy.Authentication.Token;
+using Nancy.Authentication.Token.Storage;
+using Shared.BuildingBlocks.Api.Security;
+using Tokenizer = Shared.BuildingBlocks.Api.Security.Tokenizer;
+
+namespace Shared.BuildingBlocks.Api.Installers
+{
+    public class AuthInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<ITokenizer>().UsingFactoryMethod(() => new Tokenizer(cfg => cfg.AdditionalItems().WithKeyCache(new FileSystemTokenKeyStore(new RootPathProvider())))).LifestylePerWebRequest());
+        }
+    }
+}
