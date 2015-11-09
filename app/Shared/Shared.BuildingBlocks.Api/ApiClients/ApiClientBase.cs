05eee6de0a4b4f3c27b27e90888a669e80ce91f6
bakerbrian7 <brian@customapp.co.za>
2015-11-09 11:53:50 +0200

    Added async methods

--- app/Shared/Shared.BuildingBlocks.Api/ApiClients/ApiClientBase.cs
+++ app/Shared/Shared.BuildingBlocks.Api/ApiClients/ApiClientBase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using RestSharp;
 using Shared.Configuration;
 
@@ -9,11 +11,17 @@ namespace Shared.BuildingBlocks.Api.ApiClients
     public interface IApiClient
     {
         string Get(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, params KeyValuePair<string, string>[] headers);
+        Task<string> GetAsync(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, params KeyValuePair<string, string>[] headers);
         string Post(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null, params KeyValuePair<string, string>[] headers);
+        Task<string> PostAsync(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, object body, params KeyValuePair<string, string>[] headers);
         string Put(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null, params KeyValuePair<string, string>[] headers);
+        Task<string> PutAsync(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, object body, params KeyValuePair<string, string>[] headers);
         T Get<T>(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, params KeyValuePair<string, string>[] headers) where T : new();
+        Task<T> GetAsync<T>(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, params KeyValuePair<string, string>[] headers) where T : new();
         T Post<T>(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null, params KeyValuePair<string, string>[] headers) where T : new();
+        Task<T> PostAsync<T>(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, object body, params KeyValuePair<string, string>[] headers) where T : new();
         T Put<T>(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null, params KeyValuePair<string, string>[] headers) where T : new();
+        Task<T> PutAsync<T>(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, object body, params KeyValuePair<string, string>[] headers) where T : new();
     }
 
     public abstract class ApiClientBase : IApiClient
@@ -30,8 +38,6 @@ namespace Shared.BuildingBlocks.Api.ApiClients
             _client.AddHandler("application/json", new RestSharpDataContractJsonDeserializer());
         }
 
-        
-
         public T Get<T>(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, params KeyValuePair<string, string>[] headers) where T : new()
         {
             var request = RestRequest(token, resource, Method.GET, DataFormat.Json, parameters, null, headers);
@@ -39,11 +45,27 @@ namespace Shared.BuildingBlocks.Api.ApiClients
             return _client.Execute<T>(request).Data;
         }
 
+        public async Task<T> GetAsync<T>(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, params KeyValuePair<string, string>[] headers) where T : new()
+        {
+            var request = RestRequest(authToken, resource, Method.GET, DataFormat.Json, parameters, null, headers);
+            var executeGetTaskAsync = await _client.ExecuteGetTaskAsync<T>(request, token);
+
+            return executeGetTaskAsync.Data;
+        }
+
         public T Post<T>(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null, params KeyValuePair<string, string>[] headers) where T : new()
         {
             var request = RestRequest(token, resource, Method.POST, DataFormat.Json, parameters, body, headers);
 
             return _client.Execute<T>(request).Data;
+        }
+
+        public async Task<T> PostAsync<T>(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, object body, params KeyValuePair<string, string>[] headers) where T : new()
+        {
+            var request = RestRequest(authToken, resource, Method.POST, DataFormat.Json, parameters, body, headers);
+            var executePostTaskAsync = await _client.ExecutePostTaskAsync<T>(request, token);
+
+            return executePostTaskAsync.Data;
         }
 
         public T Put<T>(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null,
@@ -54,10 +76,27 @@ namespace Shared.BuildingBlocks.Api.ApiClients
             return _client.Execute<T>(request).Data;
         }
 
+        public async Task<T> PutAsync<T>(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, object body, params KeyValuePair<string, string>[] headers) where T : new()
+        {
+            var request = RestRequest(authToken, resource, Method.PUT, DataFormat.Json, parameters, body, headers);
+            var executePostTaskAsync = await _client.ExecutePostTaskAsync<T>(request, token);
+
+            return executePostTaskAsync.Data;
+        }
+
         public string Get(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, params KeyValuePair<string, string>[] headers)
         {
             var request = RestRequest(token, resource, Method.GET, DataFormat.Json, parameters, null, headers);
+
             return _client.Execute(request).Content;
+        }
+
+        public async Task<string> GetAsync(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, params KeyValuePair<string, string>[] headers)
+        {
+            var request = RestRequest(authToken, resource, Method.GET, DataFormat.Json, parameters, null, headers);
+            var executeGetTaskAsync = await _client.ExecuteGetTaskAsync(request, token);
+
+            return executeGetTaskAsync.Content;
         }
 
         public string Post(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null, params KeyValuePair<string, string>[] headers)
@@ -66,11 +105,27 @@ namespace Shared.BuildingBlocks.Api.ApiClients
             return _client.Execute(request).Content;
         }
 
+        public async Task<string> PostAsync(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, object body, params KeyValuePair<string, string>[] headers)
+        {
+            var request = RestRequest(authToken, resource, Method.POST, DataFormat.Json, parameters, body, headers);
+            var executePostTaskAsync = await _client.ExecutePostTaskAsync(request, token);
+
+            return executePostTaskAsync.Content;
+        }
+
         public string Put(string token, string resource = "", IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null,
             params KeyValuePair<string, string>[] headers)
         {
             var request = RestRequest(token, resource, Method.PUT, DataFormat.Json, parameters, body, headers);
             return _client.Execute(request).Content;
+        }
+
+        public async Task<string> PutAsync(string authToken, CancellationToken token, string resource, IEnumerable<KeyValuePair<string, string>> parameters, object body, params KeyValuePair<string, string>[] headers)
+        {
+            var request = RestRequest(authToken, resource, Method.PUT, DataFormat.Json, parameters, body, headers);
+            var executePostTaskAsync = await _client.ExecutePostTaskAsync(request, token);
+
+            return executePostTaskAsync.Content;
         }
 
         private static RestRequest RestRequest(string token, string resource = "", Method method = Method.GET, DataFormat dataFormat = DataFormat.Json, IEnumerable<KeyValuePair<string, string>> parameters = null, object body = null, params KeyValuePair<string, string>[] headers)
