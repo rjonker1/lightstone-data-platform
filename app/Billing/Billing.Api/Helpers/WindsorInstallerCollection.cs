0545db2ec860fe110fcef4aaf62b692b30e33c77
Allistaire Clark <allistairec@lightstone.co.za>
2015-11-17 14:15:29 +0200

    Updated CIA, UM, Billing, API project refs. Added base CoS modules for Billing

--- app/Billing/Billing.Api/Helpers/WindsorInstallerCollection.cs
+++ app/Billing/Billing.Api/Helpers/WindsorInstallerCollection.cs
@@ -17,7 +17,6 @@ namespace Billing.Api.Helpers
             new UpdateBillingTransactionInstaller(),
             new AuthInstaller(),
             new ApiClientInstaller(),
-
         };
     }
 }
---
+++ app/Billing/Billing.Api/Modules/PreBIllingCoSModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+using Shared.BuildingBlocks.Api.Security;
+
+namespace Billing.Api.Modules
+{
+    public class PreBillingCoSModule : SecureModule
+    {
+        public PreBillingCoSModule()
+        {
+            Get["/PreBillingCoS"] = _ => Negotiate.WithView("Index");
+        }
+    }
+}
