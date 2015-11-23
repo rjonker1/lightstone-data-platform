ec2c5cbe4cdc009d292da6e106217a3a625b437c
Allistaire Clark <allistairec@lightstone.co.za>
2015-11-23 15:41:09 +0200

    Reporting - Added CustomerClientStatement

---
+++ app/Reporting/Workflow.Reporting/Entities/CustomerClientStatement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Workflow.Reporting.Entities
+{
+    public class CustomerClientStatement
+    {
+        public string StatementPeriod { get; set; }
+        public string CustomerClientName { get; set; }
+        public string ConsultantName { get; set; }
+        public string AccountContact { get; set; }
+        public string AccountNumber { get; set; }
+        public string ContractName { get; set; }
+        public List<PricingSummary> PricingSummaries { get; set; } 
+    }
+
+    public class PricingSummary
+    {
+        public string ContractName { get; set; }
+        public string PackageName { get; set; }
+        public string Description { get; set; }
+    }
+}
