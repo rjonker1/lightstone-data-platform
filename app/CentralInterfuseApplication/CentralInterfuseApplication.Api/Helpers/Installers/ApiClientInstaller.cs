fa1af0c5a23351f4122df64cf28c08774789a5e9
bakerbrian7 <brian@customapp.co.za>
2015-12-07 09:02:19 +0200

    Updated packages to enhance Tokenizer logging

--- app/CentralInterfuseApplication/CentralInterfuseApplication.Api/Helpers/Installers/ApiClientInstaller.cs
+++ app/CentralInterfuseApplication/CentralInterfuseApplication.Api/Helpers/Installers/ApiClientInstaller.cs
@@ -1,7 +1,6 @@
 ﻿using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
-using DataPlatform.Shared.Helpers.Extensions;
 using Shared.BuildingBlocks.Api.ApiClients;
 
 namespace CentralInterfuseApplication.Api.Helpers.Installers
@@ -10,10 +9,8 @@ namespace CentralInterfuseApplication.Api.Helpers.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            this.Info(() => "Attempting to install ApiClientInstaller");
             container.Register(Component.For<IUserManagementApiClient>().ImplementedBy<UserManagementApiClient>().LifestyleTransient());
             container.Register(Component.For<IPackageBuilderApiClient>().ImplementedBy<PackageBuilderApiClient>().LifestyleTransient());
-            this.Info(() => "Successfully installed ApiClientInstaller");
         }
     }
 }
--- app/CentralInterfuseApplication/CentralInterfuseApplication.Api/Modules/AuthModule.cs
+++ app/CentralInterfuseApplication/CentralInterfuseApplication.Api/Modules/AuthModule.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Nancy;
 using Nancy.Authentication.Forms;
 using Nancy.Authentication.Token;
