7d833921b27b56c6bab02f115136cbd5a4ae75c9
Allistaire Clark <allistairec@lightstone.co.za>
2015-12-08 10:02:43 +0200

    Shared updates

--- app/Shared/Shared/Repositories/IStoredProcedure.cs
+++ app/Shared/Shared/Repositories/IStoredProcedure.cs
@@ -4,7 +4,7 @@ namespace DataPlatform.Shared.Repositories
 {
     public interface IStoredProcedure<T>
     {
-        T StoredProcedure(string query);
+        T Query(string query);
         T WithParameters(Dictionary<string, string> queryParams);
         void Execute();
     }
